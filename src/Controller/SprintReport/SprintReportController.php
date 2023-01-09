b0c3c50f920f859942dfc3324ef1a5a582767ed8
Troels Ugilt Jensen <6103205+tuj@users.noreply.github.com>
2023-01-09 15:37:22 +0100

    JE-355: Removed commented out code

--- src/Controller/SprintReport/SprintReportController.php
+++ src/Controller/SprintReport/SprintReportController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Controller;
+namespace App\Controller\SprintReport;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
