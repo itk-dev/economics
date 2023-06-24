b4730c17ac03f17525eaaa595a77483d56a572d5
Troels Ugilt Jensen <6103205+tuj@users.noreply.github.com>
2023-06-24 11:01:59 +0200

    JE-371: Added project sync action in project overview

---
+++ assets/controllers/sync_controller.js
@@ -0,0 +1,45 @@
+import {Controller} from '@hotwired/stimulus';
+
+/**
+ * Project sync controller.
+ *
+ * Sync a project
+ */
+export default class extends Controller {
+    static targets = ['text', 'button'];
+
+    updateUrl;
+
+    connect() {
+        this.updateUrl = this.element.dataset.updateUrl;
+    }
+
+    sync() {
+        this.textTarget.innerText = 'Syncing...';
+        this.buttonTarget.style.display = 'none';
+
+        fetch(this.updateUrl, {
+            method: 'POST',
+            mode: 'same-origin',
+            cache: 'no-cache',
+            credentials: 'same-origin',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            redirect: 'follow',
+            referrerPolicy: 'no-referrer',
+        }).then(async (resp) => {
+            if (!resp.ok) {
+                resp.json().then((err) => {
+                    this.textTarget.innerText = 'failed: ' + err.message;
+                });
+            } else {
+                this.textTarget.innerText = 'ok';
+            }
+        }).catch((err) => {
+            this.textTarget.innerText = 'failed' + err.message;
+        }).finally(() => {
+            this.buttonTarget.style.display = 'block';
+        });
+    }
+}
