d99277fa1e12982d629958bf3e0d1efe9ea5bb76
Ture Gjørup <ture@ignatz.dk>
2023-07-12 11:41:42 +0200

    JE-386: Added Controller for OIDC redirect endpoint

---
+++ src/Controller/OpenIdConnectController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+class OpenIdConnectController extends AbstractController
+{
+    #[Route('/openid-connect/generic', name: 'app_openid-connect_generic')]
+    public function generic(): RedirectResponse
+    {
+        return $this->redirectToRoute('admin');
+    }
+
+    #[Route('/logout', name: 'app_logout', methods: ['GET'])]
+    public function logout()
+    {
+        // controller can be blank: it will never be called!
+        throw new \Exception('Don\'t forget to activate logout in security.yaml');
+    }
+}
