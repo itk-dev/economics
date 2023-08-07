39c3ff54be44e9f9194c13fc2806d6d6f2d2e742
jekuaitk <jekua@aarhus.dk>
2023-08-07 09:50:26 +0200

    SERV-699: Applied coding standards

--- src/Security/AzureOIDCAuthenticator.php
+++ src/Security/AzureOIDCAuthenticator.php
@@ -10,7 +10,6 @@ use ItkDev\OpenIdConnectBundle\Security\OpenIdConfigurationProviderManager;
 use ItkDev\OpenIdConnectBundle\Security\OpenIdLoginAuthenticator;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
