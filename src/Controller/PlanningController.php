afaf0fabf39c2604630404b59f6f18a3172066e5
Jeppe Krogh <kjej@aarhus.dk>
2023-12-12 12:03:19 +0100

    coding standards

--- src/Controller/PlanningController.php
+++ src/Controller/PlanningController.php
@@ -3,9 +3,9 @@
 namespace App\Controller;
 
 use App\Service\ProjectTrackerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 #[Route('/admin/planning')]
 class PlanningController extends AbstractController
