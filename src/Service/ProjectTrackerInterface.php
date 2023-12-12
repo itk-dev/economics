c34403ac6657f01698afe38949a362ec1cc95e44
Jeppe Krogh <kjej@aarhus.dk>
2023-12-12 14:28:00 +0100

    trimmed projecttrackerinterface

--- src/Service/ProjectTrackerInterface.php
+++ src/Service/ProjectTrackerInterface.php
@@ -10,23 +10,8 @@ use App\Model\SprintReport\SprintReportVersions;
 
 interface ProjectTrackerInterface
 {
-    // public function getEndpoints(): array;
-
-    // public function getAllProjects(): mixed;
-
-    // public function getProject($key): mixed;
-
-    // public function getMilestone($key): mixed;
-
-    // public function getProjectMilestones($key): mixed;
-
-    // public function getAllSprints(): array;
-
-    // public function getTicketsInSprint(string $sprintId): array;
 
     public function getPlanningData(): PlanningData;
-
-    // public function getTimesheetsForTicket($ticketId): mixed;
 
     public function getSprintReportData(string $projectId, string $versionId): SprintReportData;
 
