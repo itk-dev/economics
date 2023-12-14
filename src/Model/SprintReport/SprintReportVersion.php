9986a87dc5b67ce10a0846eba97636865971fcaf
Jeppe Krogh <kjej@aarhus.dk>
2023-12-14 13:27:22 +0100

    undid model construct changes

--- src/Model/SprintReport/SprintReportVersion.php
+++ src/Model/SprintReport/SprintReportVersion.php
@@ -7,11 +7,4 @@ class SprintReportVersion
     public string $id;
     public string $name;
     public string $projectTrackerId;
-
-    public function __construct(string $id, string $name, string $projectTrackerId)
-    {
-        $this->id = $id;
-        $this->name = $name;
-        $this->projectTrackerId = $projectTrackerId;
-    }
 }
