ac07ea9739aed24acf41d17017084d15120420fe
Troels Ugilt Jensen <6103205+tuj@users.noreply.github.com>
2023-12-15 15:30:09 +0100

    JE-400: Applied coding standards

--- src/Model/Invoices/PagedResult.php
+++ src/Model/Invoices/PagedResult.php
@@ -5,11 +5,10 @@ namespace App\Model\Invoices;
 class PagedResult
 {
     public function __construct(
-      public readonly array $items,
-      public readonly int $startAt,
-      public readonly int $maxResults,
-      public readonly int $total
-    )
-    {
+        public readonly array $items,
+        public readonly int $startAt,
+        public readonly int $maxResults,
+        public readonly int $total
+    ) {
     }
 }
