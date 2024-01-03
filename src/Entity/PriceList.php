ffec7bd56b1eda707a6e16621d3f117751ffe04c
Jeppe Krogh <kjej@aarhus.dk>
2024-01-03 13:47:32 +0100

    adding pricelist entity

---
+++ src/Entity/PriceList.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PriceListRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: PriceListRepository::class)]
+class PriceList
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    private ?string $name = null;
+
+    private ?float $price = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?int
+    {
+        return $this->name;
+    }
+    public function getPrice(): ?int
+    {
+        return $this->price;
+    }
+}
