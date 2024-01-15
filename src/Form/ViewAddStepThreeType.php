5e3ab52c92ebcd7e6ddda0a9be53f2034b2dd57d
martinyde <yde001@gmail.com>
2024-01-15 10:07:47 +0100

    Modified stuff after code review

--- src/Form/ViewAddStepThreeType.php
+++ src/Form/ViewAddStepThreeType.php
@@ -2,9 +2,7 @@
 
 namespace App\Form;
 
-use App\Entity\Invoice;
 use App\Entity\View;
-use Doctrine\ORM\EntityRepository;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
