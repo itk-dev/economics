0c51d5ef1785bc78215d40ea44f90501745b1cd2
Troels Ugilt Jensen <6103205+tuj@users.noreply.github.com>
2024-02-07 10:45:22 +0100

    #566: Fixed minor issues

--- assets/controllers/navigation_controller.js
+++ assets/controllers/navigation_controller.js
@@ -1,4 +1,4 @@
-import { Controller } from "@hotwired/stimulus";
+import {Controller} from "@hotwired/stimulus";
 
 /*
  * The following line makes this controller "lazy": it won't be downloaded until needed
@@ -6,32 +6,31 @@ import { Controller } from "@hotwired/stimulus";
  */
 /* stimulusFetch: 'lazy' */
 export default class extends Controller {
-    
-  connect() {
-    // Find active menupoint
-    const currentPath = window.location.pathname;
-    const pathPattern = /\/admin\/(?:[^/]+\/)?([^/]+)/;
+    connect() {
+        // Find active menupoint
+        const currentPath = window.location.pathname;
+        const pathPattern = /\/admin\/(?:[^/]+\/)?([^/]+)/;
 
-    const pathMatch = currentPath.match(pathPattern);
-    let activeNavigationElement;
+        const pathMatch = currentPath.match(pathPattern);
+        let activeNavigationElement;
 
-    if (pathMatch) {
-      activeNavigationElement = document.querySelector('a.navigation-item[href="' + pathMatch[0] + '/"]');
-    } else {
-      activeNavigationElement = document.querySelector('a[href*="/admin/"]');
+        if (pathMatch) {
+            activeNavigationElement = document.querySelector('a.navigation-item[href="' + pathMatch[0] + '/"]');
+        } else {
+            activeNavigationElement = document.querySelector('a[href*="/admin/"]');
+        }
+
+        activeNavigationElement.classList.add("current");
+
+        const activeElementParent = activeNavigationElement.closest(".navigation-item-submenu");
+
+        // Expand collapsible if active menu point is child
+        if (activeElementParent) {
+            activeElementParent.classList.add("shown");
+        }
     }
 
-    activeNavigationElement.classList.add("current");
-
-    const activeElementParent = activeNavigationElement.closest(".navigation-item-submenu");
-
-    // Expand collapsible if active menu point is child
-    if (activeElementParent) {
-      activeElementParent.classList.add("shown");
+    toggle(target) {
+        target.currentTarget.nextElementSibling.classList.toggle("shown");
     }
-  }
-
-  toggle(target) {
-    target.currentTarget.nextElementSibling.classList.toggle("shown");
-  }
 }
