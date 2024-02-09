094c3cfb49ee692e8e0495938f79b861b651cb38
Troels Ugilt Jensen <6103205+tuj@users.noreply.github.com>
2024-02-09 11:51:59 +0100

    #585: Applied coding standards

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -2,7 +2,7 @@
 
 $finder = (new PhpCsFixer\Finder())
     ->in(__DIR__)
-    ->exclude('var')
+    ->exclude(['var', 'node_modules'])
 ;
 
 return (new PhpCsFixer\Config())
--- src/Form/ClientType.php
+++ src/Form/ClientType.php
@@ -5,7 +5,6 @@ namespace App\Form;
 use App\Entity\Client;
 use App\Enum\ClientTypeEnum;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\EnumType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
