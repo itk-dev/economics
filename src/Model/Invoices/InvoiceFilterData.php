58b129967196353cebcf1a120c00eb51ce3ec4de
Mikkel Ricky <rimi@aarhus.dk>
2024-04-19 13:05:54 +0200

    1207: Added invoice query

--- src/Model/Invoices/InvoiceFilterData.php
+++ src/Model/Invoices/InvoiceFilterData.php
@@ -4,6 +4,7 @@ namespace App\Model\Invoices;
 
 class InvoiceFilterData
 {
+    public ?string $query = '';
     public ?bool $recorded = false;
     public ?string $createdBy = '';
     public ?bool $projectBilling = false;
