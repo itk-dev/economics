41fd03729c45642473f7b44a773693f951a6a0db
Troels Ugilt Jensen <6103205+tuj@users.noreply.github.com>
2024-05-02 11:32:08 +0200

    1258: Setup font awesome icon loader

---
+++ assets/fontawesome.js
@@ -0,0 +1,10 @@
+// Import the svg core library
+import { library, dom } from '@fortawesome/fontawesome-svg-core'
+
+// Import the icons from the free solid package.
+import {
+    faMaximize,
+} from '@fortawesome/free-solid-svg-icons'
+
+library.add(faMaximize);
+dom.i2svg()
