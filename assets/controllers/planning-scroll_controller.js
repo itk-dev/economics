3c081bc533aeed580ff00ab7b4671954e1b851e9
Jesper Pedersen <jesper@yepz.dk>
2024-05-03 13:02:01 +0200

    Apply coding standars

--- assets/controllers/planning-scroll_controller.js
+++ assets/controllers/planning-scroll_controller.js
@@ -1,8 +1,6 @@
 import { Controller } from "@hotwired/stimulus";
 
-/**
- * Scroll to active sprint in planning view
- */
+/** Scroll to active sprint in planning view */
 export default class extends Controller {
     static targets = ["column"];
 
@@ -15,15 +13,19 @@ export default class extends Controller {
 
         const activeIndex = activeColumn.dataset.index;
         const scrollToIndex = Math.max(0, activeIndex - 1);
-        const scrollToColumn = this.columnTargets.find((el) => el.dataset.index == scrollToIndex );
-        const firstColumn = this.columnTargets.find((el) => el.dataset.index == 1 );
+        const scrollToColumn = this.columnTargets.find(
+            (el) => el.dataset.index == scrollToIndex,
+        );
+        const firstColumn = this.columnTargets.find(
+            (el) => el.dataset.index == 1,
+        );
 
         if (!scrollToColumn || !firstColumn) {
             return;
         }
 
-        const { x } = scrollToColumn.getBoundingClientRect()
-        const { x: firstColumnX } = firstColumn.getBoundingClientRect()
+        const { x } = scrollToColumn.getBoundingClientRect();
+        const { x: firstColumnX } = firstColumn.getBoundingClientRect();
 
         scrollContainer.scrollTo(x - firstColumnX, 0);
     }
--- assets/fontawesome.js
+++ assets/fontawesome.js
@@ -1,16 +1,12 @@
 // Import the svg core library
-import { library, dom } from '@fortawesome/fontawesome-svg-core'
+import { library, dom } from "@fortawesome/fontawesome-svg-core";
 
 // Import the icons from the free solid package.
 import {
     faMaximize,
     faEyeSlash,
     faMinimize,
-} from '@fortawesome/free-solid-svg-icons'
+} from "@fortawesome/free-solid-svg-icons";
 
-library.add(
-    faMaximize,
-    faEyeSlash,
-    faMinimize,
-);
-dom.i2svg()
+library.add(faMaximize, faEyeSlash, faMinimize);
+dom.i2svg();
