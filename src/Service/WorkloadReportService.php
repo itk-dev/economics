f447d6b8bf56cf12843530c219fbe40c8fccc49d
Jeppe Krogh <kjej@aarhus.dk>
2024-06-04 15:32:25 +0200

    modified workload report service to get data split into weeks

--- src/Service/WorkloadReportService.php
+++ src/Service/WorkloadReportService.php
@@ -13,29 +13,41 @@ class WorkloadReportService
     public function __construct(
         private readonly WorkerRepository $workerRepository,
         private readonly WorklogRepository $worklogRepository,
+        private readonly DateTimeHelper $dateTimeHelper,
     ) {
     }
 
     /**
      * @throws EconomicsException
+     * @throws \Exception
      */
     public function getWorkloadReport(): WorkloadReportData
     {
         $workloadReportData = new WorkloadReportData();
 
         $workers = $this->workerRepository->findAll();
+        $weeksOfTheYear = $this->dateTimeHelper->getWeeksOfYear();
 
         foreach ($workers as $worker) {
             $workloadReportWorker = new WorkloadReportWorker();
             $workloadReportWorker->setEmail($worker->getUserIdentifier());
             $workloadReportWorker->setWorkload($worker->getWorkload());
-            $worklogs = $this->worklogRepository->findBy(['worker' => $worker->getUserIdentifier()]);
-            $loggedHours = 0;
-            foreach ($worklogs as $worklog) {
-                $loggedHours += ($worklog->getTimeSpentSeconds() / 60 / 60);
+
+            foreach ($weeksOfTheYear as $week) {
+                $firstAndLastDateOfWeek = $this->dateTimeHelper->getFirstAndLastDateOfWeek($week);
+                $firstDay = $firstAndLastDateOfWeek[0];
+                $lastDay = $firstAndLastDateOfWeek[1];
+                $worklogs = $this->worklogRepository->findWorklogsByWorkerAndStartDateRange($worker->getUserIdentifier(), $firstDay, $lastDay);
+                $loggedHours = 0;
+                foreach ($worklogs as $worklog) {
+                    $loggedHours += ($worklog->getTimeSpentSeconds() / 60 / 60);
+                }
+                $loggedPercentage = round(($loggedHours / $worker->getWorkload()) * 100);
+                $workloadReportData->yearWeeks->add($week);
+                $workloadReportWorker->hoursLogged->set($week, $loggedPercentage);
+
             }
 
-            $workloadReportWorker->setHoursLogged($loggedHours);
             $workloadReportData->workers->add($workloadReportWorker);
         }
 
