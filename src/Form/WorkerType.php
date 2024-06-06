aa9f704f198661241b579942a0aff67340e87758
Jeppe Krogh <kjej@aarhus.dk>
2024-06-06 11:21:04 +0200

    Improved the state of worker crud

--- src/Form/WorkerType.php
+++ src/Form/WorkerType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Worker;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,8 +13,22 @@ class WorkerType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            ->add('workload')
+            ->add('email', TextType::class, [
+                'label' => 'worker.email',
+                'label_attr' => ['class' => 'label'],
+                'attr' => ['class' => 'form-element'],
+                'help_attr' => ['class' => 'form-help'],
+                'required' => false,
+                'row_attr' => ['class' => 'form-row'],
+            ])
+            ->add('workload', TextType::class, [
+                'label' => 'worker.workload',
+                'label_attr' => ['class' => 'label'],
+                'attr' => ['class' => 'form-element'],
+                'help_attr' => ['class' => 'form-help'],
+                'required' => false,
+                'row_attr' => ['class' => 'form-row'],
+            ])
         ;
     }
 
