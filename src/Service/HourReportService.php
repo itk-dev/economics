bc37fb4d805616dac74df911f84c1549ebdd94e0
Jeppe Krogh <kjej@aarhus.dk>
2024-06-07 10:54:17 +0200

    Optimized getting hour report issues in cases where milestone is provided

--- src/Service/HourReportService.php
+++ src/Service/HourReportService.php
@@ -9,16 +9,12 @@ use App\Model\Reports\HourReportProjectTicket;
 use App\Model\Reports\HourReportTimesheet;
 use App\Repository\IssueRepository;
 use App\Repository\ProjectRepository;
-use App\Repository\ProjectVersionBudgetRepository;
 use App\Repository\VersionRepository;
 use App\Repository\WorklogRepository;
-use Doctrine\ORM\EntityManagerInterface;
 
 class HourReportService
 {
     public function __construct(
-        private readonly ProjectVersionBudgetRepository $budgetRepository,
-        private readonly EntityManagerInterface $entityManager,
         private readonly IssueRepository $issueRepository,
         private readonly WorklogRepository $worklogRepository,
         private readonly ProjectRepository $projectRepository,
@@ -40,14 +36,11 @@ class HourReportService
         return $projectChoices;
     }
 
-    /**
-     * @throws EconomicsException
-     */
-    public function getMilestones(string $projectId, bool $allAllOption = false): array
+    public function getMilestones(string $projectId, bool $includeAllOption = false): array
     {
         $milestones = $this->versionRepository->findBy(['project' => $projectId]);
         $milestoneChoices = [];
-        if ($allAllOption) {
+        if ($includeAllOption) {
             $milestoneChoices['All milestones'] = '0';
         }
         foreach ($milestones as $milestone) {
@@ -67,19 +60,15 @@ class HourReportService
             throw new EconomicsException('No project id specified');
         }
         $hourReportData = new HourReportData(0, 0);
-        $projectIssues = $this->issueRepository->findBy(['project' => $projectId]);
+
+        // If version is provided, we only want the issues containing the versionId
+        if ($versionId) {
+            $projectIssues = $this->issueRepository->issueContainingVersion($versionId);
+        } else {
+            $projectIssues = $this->issueRepository->findBy(['project' => $projectId]);
+        }
 
         foreach ($projectIssues as $issue) {
-            // If version is provided, we only want the issues containing the versionId
-            if ($versionId) {
-                $issueHasVersion = $issue->getVersions()->exists(function ($key, $value) use ($versionId) {
-                    return $value->getId() === $versionId;
-                });
-
-                if (!$issueHasVersion) {
-                    continue;
-                }
-            }
             $totalTicketEstimated = (float) $issue->planHours;
 
             $timesheetData = $this->worklogRepository->findBy(['issue' => $issue->getId()]);
@@ -95,7 +84,7 @@ class HourReportService
 
             $projectTicket->timesheets->add($timesheets);
 
-            $issueEpicName = $issue->getEpicName() ?? '';
+            $issueEpicName = (string) $issue->getEpicName() ?? '';
 
             if ($hourReportData->projectTags->containsKey($issueEpicName)) {
                 $projectTag = $hourReportData->projectTags->get($issueEpicName);
