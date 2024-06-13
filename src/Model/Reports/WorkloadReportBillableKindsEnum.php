7150f95803c842e0041b9fad4644193f0c37d108
Jeppe Krogh <kjej@aarhus.dk>
2024-06-13 10:23:58 +0200

    Added permalink to leantime repo describing billableKinds enum

--- src/Model/Reports/WorkloadReportBillableKindsEnum.php
+++ src/Model/Reports/WorkloadReportBillableKindsEnum.php
@@ -2,6 +2,10 @@
 
 namespace App\Model\Reports;
 
+/*
+ * Kind is a term on a worklog in Leantime:
+ * https://github.com/Leantime/leantime/blob/80c4542e19692e423820bd9030907070d281571e/app/Domain/Timesheets/Services/Timesheets.php#L22
+ * */
 enum WorkloadReportBillableKindsEnum: string
 {
     public const GENERAL_BILLABLE = 'GENERAL_BILLABLE';
