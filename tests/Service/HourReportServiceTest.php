e44f875edde7b3f5e2d4431541379b9d648c7785
Jeppe Krogh <kjej@aarhus.dk>
2024-06-17 15:12:39 +0200

    Added code coverage command and config

---
+++ tests/Service/HourReportServiceTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Tests\Service;
+
+use App\Service\HourReportService;
+use PHPUnit\Framework\TestCase;
+
+class HourReportServiceTest extends TestCase
+{
+    private HourReportService $hourReportService;
+
+    public function setUp(): void
+    {
+        $this->hourReportService = new HourReportService(
+            $this->createMock(IssueRepository::class),
+            $this->createMock(WorklogRepository::class),
+            $this->createMock(ProjectRepository::class),
+            $this->createMock(VersionRepository::class)
+        );
+    }
+
+    public function testGetFromDate()
+    {
+        $fromDate = $this->hourReportService->getFromDate();
+
+        $expectedFromDate = (new \DateTime())->modify('first day of this month')->format('Y-m-d');
+
+        $this->assertSame($expectedFromDate, $fromDate);
+    }
+}
