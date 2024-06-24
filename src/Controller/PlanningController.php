b74d34188e28c9a7c8b27d7c13a6569baec0215c
Jeppe Krogh <kjej@aarhus.dk>
2024-06-24 16:21:11 +0200

    Modified planningController to get data from planningService

--- src/Controller/PlanningController.php
+++ src/Controller/PlanningController.php
@@ -9,6 +9,7 @@ use App\Form\PlanningType;
 use App\Model\Planning\PlanningFormData;
 use App\Repository\DataProviderRepository;
 use App\Service\DataProviderService;
+use App\Service\PlanningService;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -24,6 +25,7 @@ class PlanningController extends AbstractController
         private readonly DataProviderService $dataProviderService,
         private readonly DataProviderRepository $dataProviderRepository,
         private readonly ?string $defaultDataProvider,
+        private readonly PlanningService $planningService,
     ) {
     }
 
@@ -84,19 +86,13 @@ class PlanningController extends AbstractController
 
         $form->handleRequest($request);
 
-        $service = null;
-
         if ($form->isSubmitted() && $form->isValid()) {
-            $service = $this->dataProviderService->getService($planningFormData->dataProvider);
-        } elseif (null !== $defaultProvider) {
-            $service = $this->dataProviderService->getService($defaultProvider);
-        }
-
-        try {
-            $planningData = $service?->getPlanningDataWeeks();
-        } catch (\Exception $e) {
-            $error = $e->getMessage();
-            $planningData = null;
+            try {
+                $planningData = $this->planningService->getPlanningData();
+            } catch (\Exception $e) {
+                $error = $e->getMessage();
+                $planningData = null;
+            }
         }
 
         return $this->render('planning/planning.html.twig', [
