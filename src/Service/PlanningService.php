c378563c814b97ef842a42127989fbd7f12d002e
Jeppe Krogh <kjej@aarhus.dk>
2024-07-01 11:28:52 +0200

    Refactored PlanningService + added weekGoalLow and weekGoalHigh values from .env

--- src/Service/PlanningService.php
+++ src/Service/PlanningService.php
@@ -17,9 +17,43 @@ class PlanningService
     public function __construct(
         private readonly DateTimeHelper $dateTimeHelper,
         private readonly IssueRepository $issueRepository,
+        protected readonly float $weekGoalLow,
+        protected readonly float $weekGoalHigh,
     ) {
     }
 
+    private const UNNAMED_STR = 'unnamed';
+
+    /**
+     * Retrieves the planning data containing the weeks, issues, assignees, and projects.
+     *
+     * @return PlanningData The planning data object
+     */
+    public function getPlanningData(): PlanningData
+    {
+        $planning = new PlanningData();
+        $planning->weeks = $this->buildPlanningWeeks($planning);
+        $allIssues = $this->issueRepository->findAll();
+        $sortedIssues = $this->sortIssuesByWeek($allIssues);
+
+        foreach ($sortedIssues as $week => $issues) {
+            $this->processIssuesForWeek($planning, $week, $issues);
+        }
+
+        $planning->assignees = $this->sortAssigneeCollectionByDisplayName($planning->assignees);
+        $planning->projects = $this->sortProjectCollectionByDisplayName($planning->projects);
+
+        return $planning;
+    }
+
+    /**
+     * Builds and returns an ArrayCollection of Weeks objects based on the given PlanningData object.
+     * Each Weeks object represents a week in the planning.
+     *
+     * @param PlanningData $planning The PlanningData object containing the weeks data
+     *
+     * @return ArrayCollection<Weeks> The ArrayCollection of Weeks objects representing the weeks in the planning
+     */
     private function buildPlanningWeeks(PlanningData $planning): ArrayCollection
     {
         $weeks = $planning->weeks;
@@ -36,8 +70,8 @@ class PlanningService
                 $supportWeek = new Weeks();
                 $supportWeek->weekCollection->add($week);
                 $supportWeek->weeks = 1;
-                $supportWeek->weekGoalLow = 25;
-                $supportWeek->weekGoalHigh = 34.5;
+                $supportWeek->weekGoalLow = $this->weekGoalLow;
+                $supportWeek->weekGoalHigh = $this->weekGoalHigh;
                 $supportWeek->displayName = (string) $week;
                 $supportWeek->dateSpan = $firstAndLastDateOfWeek['first'].' - '.$firstAndLastDateOfWeek['last'];
                 if ($week == $currentWeek) {
@@ -62,8 +96,8 @@ class PlanningService
                     $regularWeek = new Weeks();
                     $regularWeek->weekCollection->add($week);
                     $regularWeek->weeks = 1;
-                    $regularWeek->weekGoalLow = 25 * 3;
-                    $regularWeek->weekGoalHigh = 34.5 * 3;
+                    $regularWeek->weekGoalLow = $this->weekGoalLow * 3;
+                    $regularWeek->weekGoalHigh = $this->weekGoalHigh * 3;
                     $regularWeek->displayName = (string) $week;
                     $regularWeek->dateSpan = $firstAndLastDateOfWeek['first'];
                     if ($week == $currentWeek) {
@@ -76,15 +110,16 @@ class PlanningService
         return $weeks;
     }
 
-    public function getPlanningData(): PlanningData
+    /**
+     * Sorts issues by week.
+     *
+     * @param array $allIssues the array of all issues to sort
+     *
+     * @return array the sorted array of issues
+     */
+    private function sortIssuesByWeek(array $allIssues): array
     {
-        $planning = new PlanningData();
-        $planning->weeks = $this->buildPlanningWeeks($planning);
-        $assignees = $planning->assignees;
-        $projects = $planning->projects;
-
         $weekIssues = [];
-        $allIssues = $this->issueRepository->findAll();
 
         foreach ($allIssues as $issue) {
             $issueDueDate = $issue->getDueDate();
@@ -96,146 +131,198 @@ class PlanningService
             $issueYear = $issueDueDate->format('Y');
             $issueWeek = (int) $issueDueDate->format('W');
 
-            if ('-0001' !== $issueYear) {
+            if ($issueYear) {
                 $weekIssues[$issueWeek][] = $issue;
             } else {
                 $weekIssues['unscheduled'][] = $issue;
             }
         }
 
-        foreach ($weekIssues as $week => $issues) {
-            foreach ($issues as $issueData) {
-                if ('0' !== $issueData->getStatus()) { // excludes done issues.
-                    $week = (string) $week;
-                    $issueProject = $issueData->getProject();
-                    if (!$issueProject) {
-                        continue;
-                    }
-                    $projectKey = (string) $issueProject->getProjectTrackerId();
-                    $projectDisplayName = $issueProject->getName() ?? 'unnamed';
+        return $weekIssues;
+    }
 
-                    $hoursRemaining = $issueData->getHoursRemaining();
-                    if (empty($issueData->getWorker())) {
-                        $assigneeKey = 'unassigned';
-                        $assigneeDisplayName = 'Unassigned';
-                    } else {
-                        $assigneeKey = (string) $issueData->getWorker();
-                        $assigneeDisplayName = $assigneeKey;
-                    }
-                    // Add assignee if not already added.
-                    if (!$assignees->containsKey($assigneeKey)) {
-                        $assignees->set($assigneeKey, new Assignee($assigneeKey, $assigneeDisplayName));
-                    }
-
-                    /** @var Assignee $assignee */
-                    $assignee = $assignees->get($assigneeKey);
-
-                    // Add sprint if not already added.
-                    if (!$assignee->sprintSums->containsKey($week)) {
-                        $assignee->sprintSums->set($week, new SprintSum($week));
-                    }
-
-                    /** @var SprintSum $sprintSum */
-                    $sprintSum = $assignee->sprintSums->get($week);
-                    $sprintSum->sumHours += $hoursRemaining;
-
-                    // Add assignee project if not already added.
-                    if (!$assignee->projects->containsKey($projectKey)) {
-                        $assigneeProject = new AssigneeProject($projectKey, $projectDisplayName);
-                        $assignee->projects->set($projectKey, $assigneeProject);
-                    }
-
-                    /** @var AssigneeProject $assigneeProject */
-                    $assigneeProject = $assignee->projects->get($projectKey);
-
-                    // Add project sprint sum if not already added.
-                    if (!$assigneeProject->sprintSums->containsKey($week)) {
-                        $assigneeProject->sprintSums->set($week, new SprintSum($week));
-                    }
-
-                    /** @var SprintSum $projectSprintSum */
-                    $projectSprintSum = $assigneeProject->sprintSums->get($week);
-                    if (isset($projectSprintSum)) {
-                        $projectSprintSum->sumHours += $hoursRemaining;
-                    }
-
-                    $assigneeProject->issues->add(
-                        new Issue(
-                            (string) $issueData->getId(),
-                            $issueData->getName() ?? 'unnamed',
-                            $hoursRemaining ?? null,
-                            '/tickets/showKanban?showTicketModal='.$issueData->getId().'#/tickets/showTicket/'.$issueData->getId(),
-                            $week
-                        )
-                    );
-
-                    // Add project if not already added.
-                    if (!$projects->containsKey($projectKey)) {
-                        $projects->set($projectKey, new Project(
-                            $projectKey,
-                            $projectDisplayName,
-                        ));
-                    }
-
-                    /** @var Project $project */
-                    $project = $projects->get($projectKey);
-
-                    // Add sprint sum if not already added.
-
-                    if (!$project->sprintSums->containsKey($week)) {
-                        $project->sprintSums->set($week, new SprintSum($week));
-                    }
-
-                    /** @var SprintSum $projectSprintSum */
-                    $projectSprintSum = $project->sprintSums->get($week);
-                    $projectSprintSum->sumHours += $hoursRemaining;
-
-                    if (!$project->assignees->containsKey($assigneeKey)) {
-                        $project->assignees->set($assigneeKey, new AssigneeProject(
-                            $assigneeKey,
-                            $assigneeDisplayName,
-                        ));
-                    }
-
-                    /** @var AssigneeProject $projectAssignee */
-                    $projectAssignee = $project->assignees->get($assigneeKey);
-
-                    if (!$projectAssignee->sprintSums->containsKey($week)) {
-                        $projectAssignee->sprintSums->set($week, new SprintSum($week));
-                    }
-
-                    /** @var SprintSum $projectAssigneeSprintSum */
-                    $projectAssigneeSprintSum = $projectAssignee->sprintSums->get($week);
-                    $projectAssigneeSprintSum->sumHours += $hoursRemaining;
-
-                    $projectAssignee->issues->add(new Issue(
-                        (string) $issueData->getId(),
-                        $issueData->getName() ?? 'unnamed',
-                        isset($issueData->hourRemaining) ? $hoursRemaining : null,
-                        '/tickets/showKanban?showTicketModal='.$issueData->getId().'#/tickets/showTicket/'.$issueData->getId(),
-                        $week
-                    ));
+    private function processIssuesForWeek(PlanningData $planning, int $week, array $issues): void
+    {
+        foreach ($issues as $issueData) {
+            if ('0' !== $issueData->getStatus()) {
+                $week = (string) $week;
+                $issueProject = $issueData->getProject();
+                if (!$issueProject) {
+                    continue;
                 }
+                $projectKey = (string) $issueProject->getProjectTrackerId();
+                $projectDisplayName = $issueProject->getName() ?? self::UNNAMED_STR;
+                $hoursRemaining = $issueData->getHoursRemaining($issueData);
+                $assigneeData = $this->getAssigneeData($issueData);
+
+                $assignee = $this->getOrCreateAssignee($planning->assignees, $assigneeData);
+                $sprintSum = $this->getOrCreateSprintSum($assignee->sprintSums, $week);
+                $sprintSum->sumHours += $hoursRemaining;
+
+                $assigneeProject = $this->getOrCreateAssigneeProject($assignee->projects, $projectKey, $projectDisplayName);
+                $projectSprintSum = $this->getOrCreateSprintSum($assigneeProject->sprintSums, $week);
+                $projectSprintSum->sumHours += $hoursRemaining;
+
+                $assigneeProject->issues->add(
+                    new Issue(
+                        (string) $issueData->getId(),
+                        $issueData->getName() ?? self::UNNAMED_STR,
+                        $hoursRemaining ?? null,
+                        $issueData->getLinkToIssue(),
+                        $week
+                    )
+                );
+
+                $project = $this->getOrCreateProject($planning->projects, $projectKey, $projectDisplayName);
+                // Add sprint sum if not already added.
+                $projectSum = $this->getOrCreateSprintSum($project->sprintSums, $week);
+                $projectSum->sumHours += $hoursRemaining;
+
+                $projectAssignee = $this->getOrCreateAssigneeProject($project->assignees, $projectKey, $projectDisplayName);
+                $projectAssigneeSprintSum = $this->getOrCreateSprintSum($projectAssignee->sprintSums, $week);
+                $projectAssigneeSprintSum->sumHours += $hoursRemaining;
+
+                $projectAssignee->issues->add(new Issue(
+                    (string) $issueData->getId(),
+                    $issueData->getName() ?? 'unnamed',
+                    isset($issueData->hourRemaining) ? $hoursRemaining : null,
+                    $issueData->getLinkToIssue(),
+                    $week
+                ));
             }
         }
+    }
 
-        // Sort assignees by name.
+    /**
+     * Get the assignee key and display name.
+     *
+     * @param  mixed  $issue
+     *
+     * @return array
+     */
+    private function getAssigneeData(mixed $issue): array
+    {
+        if (empty($issue->getWorker())) {
+            return [
+                'key' => 'unassigned',
+                'displayName' => 'Unassigned',
+            ];
+        } else {
+            $assigneeKey = (string) $issue->getWorker();
+
+            return [
+                'key' => $assigneeKey,
+                'displayName' => $assigneeKey,
+            ];
+        }
+    }
+
+    /**
+     * Gets or creates an Assignee object in an ArrayCollection.
+     *
+     * @param ArrayCollection<string, Assignee> $assignees the ArrayCollection containing the Assignee objects
+     * @param array $assigneeData
+     *
+     * @return Assignee the retrieved or created Assignee object
+     */
+    private function getOrCreateAssignee(ArrayCollection $assignees, array $assigneeData): Assignee
+    {
+        if (!$assignees->containsKey($assigneeData['key'])) {
+            $assignees->set($assigneeData['key'], new Assignee($assigneeData['key'], $assigneeData['displayName']));
+        }
+
+        return $assignees->get($assigneeData['key']) ?? throw new \RuntimeException("Assignee key {$assigneeData['key']} does not exist");
+    }
+
+    /**
+     * Gets or creates a SprintSum object in an ArrayCollection.
+     *
+     * @param ArrayCollection<string, SprintSum> $sprintSums The ArrayCollection containing SprintSum objects
+     * @param string $week The week for which SprintSum object needs to be fetched or created
+     *
+     * @return SprintSum The SprintSum object corresponding to the given $week
+     */
+    private function getOrCreateSprintSum(ArrayCollection $sprintSums, string $week): SprintSum
+    {
+        if (!$sprintSums->containsKey($week)) {
+            $sprintSums->set($week, new SprintSum($week));
+        }
+
+        return $sprintSums->get($week) ?? throw new \RuntimeException("Sprint sum for week {$week} does not exist");
+    }
+
+    /**
+     * Gets or creates a Project object in an ArrayCollection.
+     *
+     * @param ArrayCollection<string, Project> $projects the ArrayCollection containing the Project objects
+     * @param string $projectKey the key of the Project object
+     * @param string $projectName the name of the Project object
+     *
+     * @return Project the retrieved or created Project object
+     */
+    private function getOrCreateProject(ArrayCollection $projects, string $projectKey, string $projectName): Project
+    {
+        if (!$projects->containsKey($projectKey)) {
+            $projects->set($projectKey, new Project(
+                $projectKey,
+                $projectName));
+        }
+
+        return $projects->get($projectKey) ?? throw new \RuntimeException("Project with key {$projectKey} does not exist");
+    }
+
+    /**
+     * Gets or creates an AssigneeProject object in an ArrayCollection.
+     *
+     * @param ArrayCollection<string, AssigneeProject> $projects The ArrayCollection containing AssigneeProject objects
+     * @param string $projectKey The key for the AssigneeProject object that needs to be fetched or created
+     * @param string $projectName The name for the AssigneeProject object that needs to be fetched or created
+     *
+     * @return AssigneeProject The AssigneeProject object corresponding to the given $projectKey
+     */
+    private function getOrCreateAssigneeProject(ArrayCollection $projects, string $projectKey, string $projectName): AssigneeProject
+    {
+        if (!$projects->containsKey($projectKey)) {
+            $assigneeProject = new AssigneeProject($projectKey, $projectName);
+            $projects->set($projectKey, $assigneeProject);
+        }
+
+        return $projects->get($projectKey) ?? throw new \RuntimeException("Assignee project with key {$projectKey} does not exist");
+    }
+
+    /**
+     * Sorts an Assignee collection by the displayName property of its elements.
+     *
+     * @param ArrayCollection<string, Assignee> $collection The ArrayCollection to be sorted
+     *
+     * @return ArrayCollection<string, Assignee> A new ArrayCollection with the sorted elements
+     */
+    private function sortAssigneeCollectionByDisplayName(ArrayCollection $collection): ArrayCollection
+    {
         /** @var \ArrayIterator $iterator */
-        $iterator = $assignees->getIterator();
+        $iterator = $collection->getIterator();
         $iterator->uasort(function ($a, $b) {
             return mb_strtolower($a->displayName) <=> mb_strtolower($b->displayName);
         });
-        $planning->assignees = new ArrayCollection(iterator_to_array($iterator));
 
-        // Sort projects by name.
+        return new ArrayCollection(iterator_to_array($iterator));
+    }
+
+    /**
+     * Sorts a Project ArrayCollection by the displayName property of its elements.
+     *
+     * @param ArrayCollection<string, Project> $collection The ArrayCollection to be sorted
+     *
+     * @return ArrayCollection<string, Project> A new ArrayCollection with the sorted elements
+     */
+    private function sortProjectCollectionByDisplayName(ArrayCollection $collection): ArrayCollection
+    {
         /** @var \ArrayIterator $iterator */
-        $iterator = $projects->getIterator();
+        $iterator = $collection->getIterator();
         $iterator->uasort(function ($a, $b) {
             return mb_strtolower($a->displayName) <=> mb_strtolower($b->displayName);
         });
 
-        $planning->projects = new ArrayCollection(iterator_to_array($iterator));
-
-        return $planning;
+        return new ArrayCollection(iterator_to_array($iterator));
     }
 }
