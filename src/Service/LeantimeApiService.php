a9917b99d0316e637bea0dee0c526c954c85c661
Mikkel Ricky <rimi@aarhus.dk>
2024-07-01 10:08:12 +0200

    1635: Added description on issue

--- src/Service/LeantimeApiService.php
+++ src/Service/LeantimeApiService.php
@@ -150,6 +150,7 @@ class LeantimeApiService implements DataProviderServiceInterface
             $issueData = new IssueData();
 
             $issueData->name = $issue->headline;
+            $issueData->description = $issue->description;
             $issueData->status = $issue->status;
             $issueData->projectTrackerId = $issue->id;
             // Leantime does not have a key for each issue.
