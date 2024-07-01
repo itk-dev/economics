7d6e198abbe5cf21d2895414da8d8a760d5ac5d0
Jeppe Krogh <kjej@aarhus.dk>
2024-07-01 12:22:38 +0200

    corrected migration

--- migrations/Version20240701102217.php
+++ migrations/Version20240701102217.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240628104750 extends AbstractMigration
+final class Version20240701102217 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -21,15 +21,11 @@ final class Version20240628104750 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE issue ADD link_to_issue VARCHAR(255) NOT NULL');
-        $this->addSql('ALTER TABLE project DROP is_billable');
-        $this->addSql('ALTER TABLE worklog DROP kind');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE worklog ADD kind VARCHAR(255) DEFAULT NULL');
         $this->addSql('ALTER TABLE issue DROP link_to_issue');
-        $this->addSql('ALTER TABLE project ADD is_billable TINYINT(1) DEFAULT NULL');
     }
 }
