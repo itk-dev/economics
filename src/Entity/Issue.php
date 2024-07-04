a46c112f49d5e08c768254b7ecb061a925b04fe9
Jeppe Krogh <kjej@aarhus.dk>
2024-07-04 12:20:46 +0200

    Changed issue status to new enum

--- src/Entity/Issue.php
+++ src/Entity/Issue.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use App\Entity\Trait\DataProviderTrait;
+use App\Enum\IssueStatusEnum;
 use App\Repository\IssueRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
@@ -19,7 +20,7 @@ class Issue extends AbstractBaseEntity
     private ?string $name = null;
 
     #[ORM\Column(length: 255)]
-    private ?string $status = null;
+    private ?IssueStatusEnum $status = null;
 
     #[ORM\Column(length: 255, nullable: true)]
     private ?string $accountKey = null;
@@ -89,12 +90,12 @@ class Issue extends AbstractBaseEntity
         return $this;
     }
 
-    public function getStatus(): ?string
+    public function getStatus(): ?IssueStatusEnum
     {
         return $this->status;
     }
 
-    public function setStatus(string $status): self
+    public function setStatus(IssueStatusEnum $status): self
     {
         $this->status = $status;
 
