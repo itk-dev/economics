bdc7dfac3ef99483d62e65821c2c1ca621f7c221
Jeppe Krogh <kjej@aarhus.dk>
2024-07-04 09:57:17 +0200

    merged migrations

--- migrations/Version20240704075704.php
+++ migrations/Version20240704075704.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240702073015 extends AbstractMigration
+final class Version20240704075704 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,14 @@ final class Version20240702073015 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE worklog CHANGE kind kind VARCHAR(255) DEFAULT NULL');
+        $this->addSql('ALTER TABLE project ADD is_billable TINYINT(1) DEFAULT NULL');
+        $this->addSql('ALTER TABLE worklog ADD kind VARCHAR(255) DEFAULT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE worklog CHANGE kind kind VARCHAR(255) NOT NULL');
+        $this->addSql('ALTER TABLE worklog DROP kind');
+        $this->addSql('ALTER TABLE project DROP is_billable');
     }
 }
