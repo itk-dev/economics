b279a88bd3ae7fcf44cfeeea69922ddbce40fdc0
Jeppe Krogh <kjej@aarhus.dk>
2024-07-04 12:21:06 +0200

    Corrected some things in enum and enumtype

---
+++ src/Enum/IssueStatusEnum.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Enum;
+
+/*
+Structure grabbed from Leantime:
+https://github.com/Leantime/leantime/blob/f81a5cbac4377f43719f77f909c79c15a0113f0a/app/Domain/Tickets/Repositories/Tickets.php#L48
+*/
+
+enum IssueStatusEnum: string
+{
+    case NEW = 'new';
+    case IN_PROGRESS = 'in progress';
+    case WAITING = 'waiting';
+    case BLOCKED = 'blocked';
+    case DONE = 'done';
+    case ARCHIVED = 'archived';
+}
