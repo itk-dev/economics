17e43c126385352325340de0dbd5b0b2e103f46c
Jeppe Krogh <kjej@aarhus.dk>
2024-08-08 14:02:13 +0200

    Added styling and improved javascript handling button

--- assets/controllers/invoice-sync-worklogs_controller.js
+++ assets/controllers/invoice-sync-worklogs_controller.js
@@ -3,16 +3,19 @@ import { Controller } from "@hotwired/stimulus";
 export default class extends Controller {
     updateUrl;
     connect() {
-        console.log('Hello from connect');
         this.updateUrl = this.element.dataset.updateUrl;
     }
 
     syncWorklogs(e) {
         let syncButton = e.target;
+        let originalText = syncButton.dataset.originaltext;
+        let successText = syncButton.dataset.successtext;
         syncButton.disabled = true;
-        syncButton.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">\n' +
-            '  <path strokeLinecap="round" strokeLinejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99" />\n' +
-            '</svg>'
+        syncButton.innerHTML = `
+<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
+    <path strokeLinecap="round" strokeLinejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99" />
+</svg>
+`;
 
         fetch(this.updateUrl, {
             method: "POST",
@@ -28,18 +31,25 @@ export default class extends Controller {
             .then(async (resp) => {
                 if (!resp.ok) {
                     resp.json().then((err) => {
+                        syncButton.classList.add("btn-danger");
                         syncButton.innerText = `failed: ${err.message}`;
                     });
                 } else {
-                    syncButton.innerText = "ok";
+                    syncButton.innerText = successText;
+                    syncButton.classList.add("btn-success");
                 }
             })
             .catch((err) => {
-                syncButton.innerText = `failed${err.message}`;
-                syncButton.disabled = false;
+                syncButton.classList.add("btn-danger");
+                syncButton.innerText = `failed: ${err.message}`;
             })
             .finally(() => {
-               syncButton.disabled = false;
+                setTimeout(() => {
+                    syncButton.disabled = false;
+                    syncButton.classList.remove("btn-success");
+                    syncButton.innerText = originalText;
+                }, 3000);
+
             });
     }
 }
--- src/Form/InvoiceEntryWorklogFilterType.php
+++ src/Form/InvoiceEntryWorklogFilterType.php
@@ -5,7 +5,6 @@ namespace App\Form;
 use App\Model\Invoices\InvoiceEntryWorklogsFilterData;
 use App\Repository\InvoiceRepository;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\ButtonType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\FormBuilderInterface;
