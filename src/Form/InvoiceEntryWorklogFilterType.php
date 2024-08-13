0a3a998a25748c5fdf7f01cfc2688207527dc301
Jeppe Krogh <kjej@aarhus.dk>
2024-08-13 11:21:10 +0200

    Coding standards

--- src/Form/InvoiceEntryWorklogFilterType.php
+++ src/Form/InvoiceEntryWorklogFilterType.php
@@ -3,7 +3,6 @@
 namespace App\Form;
 
 use App\Model\Invoices\InvoiceEntryWorklogsFilterData;
-use App\Repository\InvoiceRepository;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
