c2020d4d7717bdd3397478c6abdff7e073ea11ec
Jeppe Krogh <kjej@aarhus.dk>
2024-09-24 10:47:56 +0200

    Update AppExtension to support new functionalities

---
+++ src/Twig/AppExtension.php
@@ -0,0 +1,23 @@
+<?php
+
+// src/Twig/AppExtension.php
+
+namespace App\Twig;
+
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
+
+class AppExtension extends AbstractExtension
+{
+    public function getFilters()
+    {
+        return [
+            new TwigFilter('json_decode', [$this, 'jsonDecode']),
+        ];
+    }
+
+    public function jsonDecode($json)
+    {
+        return json_decode($json, true);
+    }
+}
