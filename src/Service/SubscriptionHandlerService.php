55c73778a8b9d508c4257cfdfbf78b4efeaf5e39
Jeppe Krogh <kjej@aarhus.dk>
2024-09-30 12:52:15 +0200

    Reformatted subscriptionHandlerService and implemented mailer

--- src/Service/SubscriptionHandlerService.php
+++ src/Service/SubscriptionHandlerService.php
@@ -2,14 +2,26 @@
 
 namespace App\Service;
 
+use App\Entity\Project;
 use App\Entity\Subscription;
+use App\Entity\Version;
 use App\Enum\SubscriptionSubjectEnum;
+use App\Exception\EconomicsException;
+use App\Model\Reports\HourReportData;
 use App\Repository\ProjectRepository;
 use App\Repository\VersionRepository;
 use Mpdf\Mpdf;
 use Mpdf\MpdfException;
 use Psr\Log\LoggerInterface;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
+use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Address;
+use Symfony\Contracts\Translation\TranslatorInterface;
 use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Error\SyntaxError;
 
 class SubscriptionHandlerService
 {
@@ -19,49 +31,191 @@ class SubscriptionHandlerService
         private readonly HourReportService $hourReportService,
         private readonly Environment $environment,
         private readonly LoggerInterface $logger,
+        private readonly MailerInterface $mailer,
+        private readonly TranslatorInterface $translator,
     ) {
     }
 
     /**
-     * Handles a subscription.
+     * Send a notification email.
      *
-     * @param Subscription $subscription the subscription to handle
-     * @param mixed $fromDate the start date of the report to generate
-     * @param mixed $toDate the end date of the report to generate
+     * @param array $notification the notification data
      *
-     * @throws MpdfException if there is an error with the PDF
+     * @return void
+     * @throws TransportExceptionInterface
      */
-    public function handleSubscription(Subscription $subscription, $fromDate, $toDate): void
+    private function sendNotification(array $notification): void
     {
-        switch ($subscription->getSubject()) {
-            case SubscriptionSubjectEnum::HOUR_REPORT:
-                $hourReport = json_decode($subscription->getUrlParams())->hour_report;
-                $projectId = $hourReport->project ?? null;
-                $project = $this->projectRepository->find($projectId);
-                if (!$project) {
-                    $this->logger->error('Project was not found with ID='.$projectId);
-                }
-                $versionId = $hourReport->version ?? null;
-                $version = null;
-                if ($versionId) {
-                    $version = $this->versionRepository->findOneBy($versionId);
-                }
-                $reportData = $this->hourReportService->getHourReport($project, $fromDate, $toDate, $version);
+        $email = (new TemplatedEmail())
+            ->from($notification['from'])
+            ->to(new Address($notification['to']))
+            ->subject($notification['subject'])
+            ->htmlTemplate($notification['template'])
+            ->attach($notification['fileAttachments']['file'], $notification['fileAttachments']['name'], 'application/pdf')
+            ->context([
+                'renderedReport' => $notification['data']['renderedReport'],
+            ]);
 
-                $renderedReport = $this->environment->render('subscription/subscription_hour_report.html.twig', [
-                    'controller_name' => 'HourReportController',
-                    'data' => $reportData,
-                    'mode' => 'hour_report',
-                    'fromDate' => $fromDate->format('d-m-Y'),
-                    'toDate' => $toDate->format('d-m-Y'),
-                ]);
+        $this->mailer->send($email);
+    }
 
-                $mpdf = new Mpdf();
-                $mpdf->WriteHTML($renderedReport);
-                $mpdf->Output('testhest'.$subscription->getId().'.pdf', \Mpdf\Output\Destination::FILE);
-                break;
-            default:
-                break;
+    /**
+     * Handles a subscription for hour report.
+     *
+     * @param Subscription $subscription the subscription object
+     * @param \DateTime $fromDate the starting date of the report
+     * @param \DateTime $toDate the ending date of the report
+     *
+     * @return void
+     * @throws EconomicsException
+     * @throws LoaderError
+     * @throws MpdfException
+     * @throws RuntimeError
+     * @throws SyntaxError
+     * @throws TransportExceptionInterface
+     */
+    public function handleSubscription(Subscription $subscription, \DateTime $fromDate, \DateTime $toDate): void
+    {
+        if (SubscriptionSubjectEnum::HOUR_REPORT !== $subscription->getSubject()) {
+            return;
         }
+
+        $email = $subscription->getEmail();
+        $params = json_decode($subscription->getUrlParams());
+        $projectId = $params->hour_report->project ?? null;
+
+        $project = $this->getProject($projectId);
+        if (!$project) {
+            return;
+        }
+
+        $version = $this->getVersion($params->hour_report->version ?? null);
+        $reportData = $this->hourReportService->getHourReport($project, $fromDate, $toDate, $version);
+        $mailData = $this->prepareMailData($subscription, $fromDate, $toDate, $project, $reportData, $email);
+
+        $this->sendNotification($mailData);
+    }
+
+    /**
+     * Get a project object by its ID.
+     *
+     * @param ?int $projectId the ID of the project, nullable
+     *
+     * @return ?Project the project object, or null if not found
+     */
+    private function getProject(?int $projectId): ?Project
+    {
+        $project = $this->projectRepository->find($projectId);
+
+        if (!$project) {
+            $this->logger->error('Project was not found with ID='.$projectId);
+        }
+
+        return $project;
+    }
+
+    /**
+     * Get a version object by id.
+     *
+     * @param int|null $versionId the id of the version
+     *
+     * @return Version|null the version object, or null if not found
+     */
+    private function getVersion(?int $versionId): ?Version
+    {
+        return $versionId ? $this->versionRepository->findOneBy((array) $versionId) : null;
+    }
+
+    /**
+     * Prepare mail data for sending a subscription hour report.
+     *
+     * @param Subscription $subscription the subscription object
+     * @param \DateTime $fromDate the starting date of the report
+     * @param \DateTime $toDate the ending date of the report
+     * @param Project $project the project object
+     * @param array $reportData the report data
+     * @param string $email the recipient email address
+     *
+     * @return array the prepared mail data
+     * @throws MpdfException
+     * @throws LoaderError
+     * @throws RuntimeError
+     * @throws SyntaxError
+     */
+    private function prepareMailData(Subscription $subscription, \DateTime $fromDate, \DateTime $toDate, Project $project, HourReportData $reportData, string $email): array
+    {
+        $renderedReport = $this->environment->render('subscription/subscription_hour_report.html.twig', [
+            'controller_name' => 'HourReportController',
+            'data' => $reportData,
+            'mode' => 'hour_report',
+            'projectName' => $project->getName(),
+            'fromDate' => $fromDate->format('d-m-Y'),
+            'toDate' => $toDate->format('d-m-Y'),
+        ]);
+
+        $attachment = $this->createPdfAttachment($renderedReport);
+        $subject = $this->createSubject($subscription, $project);
+
+        return [
+            'from' => $email,
+            'to' => $email,
+            'subject' => $subject,
+            'template' => 'email/email-subscription-hour_report.html.twig',
+            'adminNotification' => false,
+            'data' => [
+                'renderedReport' => $renderedReport,
+            ],
+            'fileAttachments' => [
+                'name' => $this->createAttachmentName($subscription, $fromDate, $toDate, $project),
+                'file' => $attachment,
+            ],
+        ];
+    }
+
+    /**
+     * Creates a PDF attachment from the given content.
+     *
+     * @param string $content the content of the PDF
+     *
+     * @return string the PDF attachment
+     *
+     * @throws MpdfException if there is an error with the PDF generation
+     */
+    private function createPdfAttachment(string $content): string
+    {
+        $mpdf = new Mpdf();
+        $mpdf->WriteHTML($content);
+
+        return $mpdf->Output('', \Mpdf\Output\Destination::STRING_RETURN);
+    }
+
+    /**
+     * Creates the subject for a subscription email.
+     *
+     * @param Subscription $subscription the subscription object
+     * @param Project $project the project object
+     *
+     * @return string the subject string for the subscription email
+     */
+    private function createSubject(Subscription $subscription, Project $project): string
+    {
+        return $this->translator->trans('subscription.subjects.'.$subscription->getSubject()->value)
+            .' - '.$project->getName()
+            .' - '.$this->translator->trans('subscription.frequencies.'.$subscription->getFrequency()->value);
+    }
+
+    /**
+     * Creates the name for the attachment file.
+     *
+     * @param Subscription $subscription the subscription object
+     * @param \DateTime $fromDate the starting date for the subscription period
+     * @param \DateTime $toDate the ending date for the subscription period
+     * @param Project $project the project object
+     *
+     * @return string the name of the attachment file
+     */
+    private function createAttachmentName(Subscription $subscription, \DateTime $fromDate, \DateTime $toDate, Project $project): string
+    {
+        return $subscription->getSubject()->value.'_'.$project->getName().'_'.$fromDate->format('d-m-Y').'-'.$toDate->format('d-m-Y').'.pdf';
     }
 }
