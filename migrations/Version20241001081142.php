108a8f0e1308527f807464e83c67259572dd8eb0
Jeppe Krogh <kjej@aarhus.dk>
2024-10-01 10:12:41 +0200

    Introduction doctrine json type on urlparams subscription entity value + Reduced migraitons

--- migrations/Version20241001081142.php
+++ migrations/Version20241001081142.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240924101242 extends AbstractMigration
+final class Version20241001081142 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,7 +20,7 @@ final class Version20240924101242 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE subscription (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, subject VARCHAR(255) DEFAULT NULL, url_params VARCHAR(180) NOT NULL, frequency VARCHAR(255) DEFAULT NULL, last_sent DATETIME DEFAULT NULL, created_by VARCHAR(255) DEFAULT NULL, updated_by VARCHAR(255) DEFAULT NULL, created_at DATETIME NOT NULL, updated_at DATETIME NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE subscription (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, subject VARCHAR(255) DEFAULT NULL, url_params JSON DEFAULT NULL COMMENT \'(DC2Type:json)\', frequency VARCHAR(255) DEFAULT NULL, last_sent DATETIME DEFAULT NULL, created_by VARCHAR(255) DEFAULT NULL, updated_by VARCHAR(255) DEFAULT NULL, created_at DATETIME NOT NULL, updated_at DATETIME NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
     }
 
     public function down(Schema $schema): void
