89cbbd857f7401cc56d7385c733b656c4447f142
Jeppe Krogh <kjej@aarhus.dk>
2024-10-23 12:47:23 +0200

    Removed DataProvider property from ForecastReportFormData

--- src/Model/Reports/ForecastReportFormData.php
+++ src/Model/Reports/ForecastReportFormData.php
@@ -6,7 +6,6 @@ use App\Entity\DataProvider;
 
 class ForecastReportFormData
 {
-    public DataProvider $dataProvider;
     public \DateTimeInterface $dateFrom;
     public \DateTimeInterface $dateTo;
 }
