ccf75faca6f2243d0c7e3f7c4213127fee5d47ed
Jeppe Krogh <kjej@aarhus.dk>
2024-11-22 15:36:15 +0100

    Add method to get paginated and filtered versions

--- src/Repository/VersionRepository.php
+++ src/Repository/VersionRepository.php
@@ -3,8 +3,11 @@
 namespace App\Repository;
 
 use App\Entity\Version;
+use App\Model\Invoices\VersionFilterData;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
+use Knp\Component\Pager\Pagination\PaginationInterface;
+use Knp\Component\Pager\PaginatorInterface;
 
 /**
  * @extends ServiceEntityRepository<Version>
@@ -16,7 +19,7 @@ use Doctrine\Persistence\ManagerRegistry;
  */
 class VersionRepository extends ServiceEntityRepository
 {
-    public function __construct(ManagerRegistry $registry)
+    public function __construct(ManagerRegistry $registry, private readonly PaginatorInterface $paginator)
     {
         parent::__construct($registry, Version::class);
     }
@@ -37,5 +40,35 @@ class VersionRepository extends ServiceEntityRepository
         if ($flush) {
             $this->getEntityManager()->flush();
         }
+    }
+
+    public function getFilteredPagination(VersionFilterData $versionFilterData, int $page = 1): PaginationInterface
+    {
+        $qb = $this->createQueryBuilder('version');
+
+        if (!is_null($versionFilterData->isBillable)) {
+            $qb->andWhere(
+                $versionFilterData->isBillable
+                    ? 'version.isBillable = TRUE'
+                    : 'version.isBillable = FALSE OR version.isBillable IS NULL'
+            );
+        }
+
+        if (!is_null($versionFilterData->name)) {
+            $name = $versionFilterData->name;
+            $qb->andWhere('version.name LIKE :name')->setParameter('name', "%$name%");
+        }
+
+        /* if (!is_null($versionFilterData->project)) {
+             $project = $versionFilterData->project;
+             $qb->andWhere('version.project LIKE :project')->setParameter('project', "%$project%");
+         }*/
+
+        return $this->paginator->paginate(
+            $qb,
+            $page,
+            50,
+            ['defaultSortFieldName' => 'version.id', 'defaultSortDirection' => 'asc']
+        );
     }
 }
