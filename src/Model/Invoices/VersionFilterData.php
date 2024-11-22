d512522033efff77c6696c932067ee2d60993cc5
Jeppe Krogh <kjej@aarhus.dk>
2024-11-22 15:36:15 +0100

    Update VersionFilterData properties

---
+++ src/Model/Invoices/VersionFilterData.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Model\Invoices;
+
+class VersionFilterData
+{
+    public ?string $id = null;
+    public ?string $name = null;
+    public ?string $project;
+    public ?bool $isBillable = null;
+}
