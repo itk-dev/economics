46dd65f601500b94c8cf6d0ac74a5604d4f1eb49
Jeppe Krogh <kjej@aarhus.dk>
2024-12-17 13:13:50 +0100

    Added ui for new includeInReports for worker

--- src/Form/WorkerType.php
+++ src/Form/WorkerType.php
@@ -4,9 +4,11 @@ namespace App\Form;
 
 use App\Entity\Worker;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Choice;
 
 class WorkerType extends AbstractType
 {
@@ -36,6 +38,20 @@ class WorkerType extends AbstractType
                 'help_attr' => ['class' => 'form-help'],
                 'required' => false,
                 'row_attr' => ['class' => 'form-row'],
+            ])
+            ->add('includeInReports', ChoiceType::class, [
+                'label' => 'worker.includeInReports',
+                'label_attr' => ['class' => 'label'],
+                'attr' => ['class' => 'form-element'],
+                'help_attr' => ['class' => 'form-help'],
+                'required' => false,
+                'row_attr' => ['class' => 'form-row'],
+                'choices' => [
+                    'Ja' => true,
+                    'Nej' => false,
+                ],
+                'multiple' => false,
+                'placeholder' => false,
             ])
         ;
     }
