666e01afc8ad3d54698eac15c00b93f0f3ff2a98
Jeppe Krogh <kjej@aarhus.dk>
2024-12-17 13:21:09 +0100

    Removed ui for versions

--- src/Repository/VersionRepository.php
+++ src/Repository/VersionRepository.php
@@ -6,7 +6,6 @@ use App\Entity\Version;
 use App\Model\Invoices\VersionFilterData;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
-use Knp\Component\Pager\Pagination\PaginationInterface;
 use Knp\Component\Pager\PaginatorInterface;
 
 /**
@@ -40,35 +39,5 @@ class VersionRepository extends ServiceEntityRepository
         if ($flush) {
             $this->getEntityManager()->flush();
         }
-    }
-
-    public function getFilteredPagination(VersionFilterData $versionFilterData, int $page = 1): PaginationInterface
-    {
-        $qb = $this->createQueryBuilder('version');
-
-        if (!is_null($versionFilterData->isBillable)) {
-            $qb->andWhere(
-                $versionFilterData->isBillable
-                    ? 'version.isBillable = TRUE'
-                    : 'version.isBillable = FALSE OR version.isBillable IS NULL'
-            );
-        }
-
-        if (!is_null($versionFilterData->name)) {
-            $name = $versionFilterData->name;
-            $qb->andWhere('version.name LIKE :name')->setParameter('name', "%$name%");
-        }
-
-        /* if (!is_null($versionFilterData->project)) {
-             $project = $versionFilterData->project;
-             $qb->andWhere('version.project LIKE :project')->setParameter('project', "%$project%");
-         }*/
-
-        return $this->paginator->paginate(
-            $qb,
-            $page,
-            50,
-            ['defaultSortFieldName' => 'version.id', 'defaultSortDirection' => 'asc']
-        );
     }
 }
