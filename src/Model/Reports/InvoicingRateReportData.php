79907e95c223fe09bdbeb93dee49cf08d71b5d0e
Jeppe Krogh <kjej@aarhus.dk>
2024-12-17 13:16:54 +0100

    Defined invoicing report data structure

---
+++ src/Model/Reports/InvoicingRateReportData.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Model\Reports;
+
+use Doctrine\Common\Collections\ArrayCollection;
+
+class InvoicingRateReportData
+{
+    public readonly string $id;
+    public readonly string $viewmode;
+    /** @var ArrayCollection<string, string> */
+    public ArrayCollection $period;
+    /** @var ArrayCollection<string, WorkloadReportWorker> */
+    public ArrayCollection $workers;
+    public int $currentPeriodNumeric;
+    public ArrayCollection $periodAverages;
+    public float $totalAverage;
+
+    public function __construct(string $viewmode)
+    {
+        $this->viewmode = $viewmode;
+        $this->period = new ArrayCollection();
+        $this->workers = new ArrayCollection();
+        $this->periodAverages = new ArrayCollection();
+        $this->totalAverage = 0;
+    }
+
+    /**
+     * Set current week.
+     *
+     * @param int $currentPeriodNumeric
+     *
+     * @return self
+     */
+    public function setCurrentPeriodNumeric(int $currentPeriodNumeric): self
+    {
+        $this->currentPeriodNumeric = $currentPeriodNumeric;
+
+        return $this;
+    }
+}
