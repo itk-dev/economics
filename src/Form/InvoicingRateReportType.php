647431b9b662a6d12ab1f1c4bcf36eda632501e4
Jeppe Krogh <kjej@aarhus.dk>
2024-12-20 12:27:46 +0100

    Integrated year and includeIssues inputs in InvoicingRateReportType form

--- src/Form/InvoicingRateReportType.php
+++ src/Form/InvoicingRateReportType.php
@@ -3,9 +3,9 @@
 namespace App\Form;
 
 use App\Model\Reports\InvoicingRateReportFormData;
-use App\Model\Reports\InvoicingRateReportViewModeEnum;
 use App\Model\Reports\WorkloadReportPeriodTypeEnum as PeriodTypeEnum;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\EnumType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -19,10 +19,15 @@ class InvoicingRateReportType extends AbstractType
 
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
-        $builder
-       /*     ->add('viewMode', EnumType::class, [
+        $yearChoices = [];
+        foreach ($options['years'] as $year) {
+            $yearChoices[$year] = $year;
+        }
+
+$builder
+            /*->add('viewMode', EnumType::class, [
                 'required' => false,
-                'label' => 'workload_report.select_viewmode',
+                'label' => 'invoicing_rate_report.select_viewmode',
                 'label_attr' => ['class' => 'label'],
                 'placeholder' => false,
                 'attr' => [
@@ -30,9 +35,20 @@ class InvoicingRateReportType extends AbstractType
                 ],
                 'class' => InvoicingRateReportViewModeEnum::class,
             ])*/
+            ->add('year', ChoiceType::class, [
+                'label' => 'invoicing_rate_report.year',
+                'label_attr' => ['class' => 'label'],
+                'attr' => ['class' => 'form-element '],
+                'help_attr' => ['class' => 'form-help'],
+                'row_attr' => ['class' => 'form-row'],
+                'required' => false,
+                'data' => $yearChoices[date('Y')],
+                'choices' => $yearChoices,
+                'placeholder' => null,
+            ])
             ->add('viewPeriodType', EnumType::class, [
                 'required' => false,
-                'label' => 'workload_report.select_view_period_type',
+                'label' => 'invoicing_rate_report.select_view_period_type',
                 'label_attr' => ['class' => 'label'],
                 'placeholder' => false,
                 'attr' => [
@@ -40,8 +56,22 @@ class InvoicingRateReportType extends AbstractType
                 ],
                 'class' => PeriodTypeEnum::class,
             ])
+            ->add('includeIssues', ChoiceType::class, [
+                'choices' => [
+                    'Ja' => true,
+                    'Nej' => false,
+                ],
+                'required' => false,
+                'placeholder' => false,
+                'label' => 'invoicing_rate_report.include_issues',
+                'label_attr' => ['class' => 'checkbox-label'],
+                'attr' => [
+                    'class' => 'form-element',
+                ],
+                'data' => false,
+            ])
             ->add('submit', SubmitType::class, [
-                'label' => 'workload_report.submit',
+                'label' => 'invoicing_rate_report.submit',
                 'attr' => [
                     'class' => 'hour-report-submit button',
                 ],
@@ -55,6 +85,7 @@ class InvoicingRateReportType extends AbstractType
             'attr' => [
                 'data-sprint-report-target' => 'form',
             ],
+            'years' => null,
         ]);
     }
 }
