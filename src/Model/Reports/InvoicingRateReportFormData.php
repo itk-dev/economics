e8eccc18941d8257176d0568f6e44e3fea36fc5f
Jeppe Krogh <kjej@aarhus.dk>
2024-12-20 12:27:47 +0100

    Added year and includeIssues fields to InvoicingRateReportFormData

--- src/Model/Reports/InvoicingRateReportFormData.php
+++ src/Model/Reports/InvoicingRateReportFormData.php
@@ -6,4 +6,6 @@ class InvoicingRateReportFormData
 {
     public WorkloadReportPeriodTypeEnum $viewPeriodType;
     public InvoicingRateReportViewModeEnum $viewMode;
+    public int $year;
+    public bool $includeIssues;
 }
