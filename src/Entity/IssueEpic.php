7b64887a450fcd8fbe31fff6c85c6c213c223340
Jeppe Krogh <kjej@aarhus.dk>
2025-01-02 11:11:09 +0100

    Added IssueEpic entity

---
+++ src/Entity/IssueEpic.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+class IssueEpic extends AbstractBaseEntity
+{
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $value = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $projectTrackerId = null;
+}
