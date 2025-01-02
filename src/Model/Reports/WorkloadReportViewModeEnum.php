fbeac62923d7eeac723b243e36154513a7c6d6ff
Jeppe Krogh <kjej@aarhus.dk>
2025-01-02 11:33:06 +0100

    Renamed workload report view mode selections

--- src/Model/Reports/WorkloadReportViewModeEnum.php
+++ src/Model/Reports/WorkloadReportViewModeEnum.php
@@ -7,14 +7,16 @@ use Symfony\Contracts\Translation\TranslatorInterface;
 
 enum WorkloadReportViewModeEnum: string implements TranslatableInterface
 {
-    case WORKLOAD = 'workload_percentage_logged';
-    case BILLABLE = 'billable_percentage_logged';
+    case WORKLOAD = 'workload';
+    case BILLABLE = 'billable';
+    case BILLED = 'billed';
 
     public function trans(TranslatorInterface $translator, ?string $locale = null): string
     {
         return match ($this) {
             self::WORKLOAD => $translator->trans('workload_report_view_mode_enum.workload.label', locale: $locale),
             self::BILLABLE => $translator->trans('workload_report_view_mode_enum.billable.label', locale: $locale),
+            self::BILLED => $translator->trans('workload_report_view_mode_enum.billed.label', locale: $locale),
         };
     }
 }
