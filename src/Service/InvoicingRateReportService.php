235fae0cc80bb6464b9af9d230af284da2ea1e72
Jeppe Krogh <kjej@aarhus.dk>
2025-01-13 12:33:00 +0100

    Coding standards

--- src/Service/InvoicingRateReportService.php
+++ src/Service/InvoicingRateReportService.php
@@ -134,7 +134,7 @@ class InvoicingRateReportService
                 $invoicingRateReportWorker->dataByPeriod->set($period, [
                     'loggedBillableHours' => $loggedBillableHours,
                     'loggedBilledPercentage' => $loggedBilledPercentage,
-                    'totalLoggedHours' => $loggedBilledHours . " / " . $loggedHours,
+                    'totalLoggedHours' => $loggedBilledHours.' / '.$loggedHours,
                 ]);
 
                 // Increment the sum and count for this period
