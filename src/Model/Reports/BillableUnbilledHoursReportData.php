3fe63d4519d1402fc0243f8ecf497aa98d5e2f5b
Jeppe Krogh <kjej@aarhus.dk>
2025-01-20 08:36:35 +0100

    Remove unused imports and clean up in BillableUnbilledHoursReportData

--- src/Model/Reports/BillableUnbilledHoursReportData.php
+++ src/Model/Reports/BillableUnbilledHoursReportData.php
@@ -3,20 +3,16 @@
 namespace App\Model\Reports;
 
 use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
 
 class BillableUnbilledHoursReportData
 {
     public string $id;
 
-
     /** @var ArrayCollection<string, string> */
     public ArrayCollection $projectData;
-
 
     public function __construct()
     {
         $this->projectData = new ArrayCollection();
     }
-
 }
