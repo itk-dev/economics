61e4134129e5f0c6651e970e5d73dd54183746fe
Jeppe Krogh <kjej@aarhus.dk>
2025-07-11 09:54:27 +0200

    Added crontask to syncCommand

--- src/Command/SyncCommand.php
+++ src/Command/SyncCommand.php
@@ -12,11 +12,13 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\Scheduler\Attribute\AsCronTask;
 
 #[AsCommand(
     name: 'app:sync',
     description: 'Sync all data.',
 )]
+#[AsCronTask(expression: '0 0 * * *', schedule: 'default')]
 class SyncCommand extends Command
 {
     public function __construct(
