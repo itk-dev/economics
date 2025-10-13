84dbb908b77775df4832125e88c12f0e3dd5edf0
Troels Ugilt Jensen <6103205+tuj@users.noreply.github.com>
2025-10-13 15:03:15 +0200

    5538: Changed sync status to reflect queue length and number of errors

--- src/Controller/SynchronizationJobController.php
+++ src/Controller/SynchronizationJobController.php
@@ -2,71 +2,32 @@
 
 namespace App\Controller;
 
-use App\Entity\SynchronizationJob;
-use App\Enum\SynchronizationStatusEnum;
-use App\Message\SynchronizeMessage;
-use App\Repository\SynchronizationJobRepository;
+use App\Service\DataProviderService;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Messenger\MessageBusInterface;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
-use Symfony\Contracts\Translation\TranslatorInterface;
 
 #[Route('/admin/synchronization')]
 class SynchronizationJobController extends AbstractController
 {
     public function __construct(
+        private readonly DataProviderService $dataProviderService,
     ) {
     }
 
     #[Route('/status', name: 'app_synchronization_status', methods: ['GET'])]
     #[IsGranted('ROLE_ADMIN')]
-    public function status(SynchronizationJobRepository $synchronizationJobRepository, TranslatorInterface $translator): Response
+    public function status(): Response
     {
-        $latestJob = $synchronizationJobRepository->getLatestJob();
-
-        if (null === $latestJob) {
-            return new JsonResponse([], Response::HTTP_NOT_FOUND);
-        }
+        $asyncLength = $this->dataProviderService->countPendingJobsByQueueName('async');
+        $failedLength = $this->dataProviderService->countFailedJobsTheLastDay();
 
         return new JsonResponse([
-            'started' => $latestJob->getStarted()?->format('c'),
-            'ended' => $latestJob->getEnded()?->format('c'),
-            'status' => $latestJob->getStatus()?->value,
-            'step' => $latestJob->getStep()?->trans($translator) ?? $latestJob->getStep()?->value,
-            'progress' => $latestJob->getProgress(),
-        ], 200);
-    }
-
-    #[Route('/start', name: 'app_synchronization_sync', methods: ['POST'])]
-    #[IsGranted('ROLE_ADMIN')]
-    public function sync(SynchronizationJobRepository $synchronizationJobRepository, MessageBusInterface $bus): Response
-    {
-        return new JsonResponse([], Response::HTTP_SERVICE_UNAVAILABLE);
-
-        //        @TODO re-enable when sync is fully re-factored
-        //        $latestJob = $synchronizationJobRepository->getLatestJob();
-        //
-        //        if (null !== $latestJob && in_array($latestJob->getStatus(), [SynchronizationStatusEnum::NOT_STARTED, SynchronizationStatusEnum::RUNNING])) {
-        //            return new JsonResponse(['message' => 'existing job'], Response::HTTP_CONFLICT);
-        //        }
-        //
-        //        $job = new SynchronizationJob();
-        //        $job->setStatus(SynchronizationStatusEnum::NOT_STARTED);
-        //        $synchronizationJobRepository->save($job, true);
-        //
-        //        $jobId = $job->getId();
-        //
-        //        if (null === $jobId) {
-        //            throw new \Exception('Job id not found');
-        //        }
-        //
-        //        $message = new SynchronizeMessage($jobId);
-        //
-        //        $bus->dispatch($message);
-        //
-        //        return new JsonResponse([], 200);
+            'async' => $asyncLength,
+            'error' => $failedLength,
+        ]);
     }
 }
