54cb641035040485d9c260f89639be2e70505262
Jeppe Krogh <kjej@aarhus.dk>
2025-10-30 15:08:37 +0100

    Set up crud functionality in service agreement controller

--- src/Controller/ServiceAgreementController.php
+++ src/Controller/ServiceAgreementController.php
@@ -3,37 +3,64 @@
 namespace App\Controller;
 
 use App\Entity\ServiceAgreement;
-use App\Form\ServiceAgreementType;
+use App\Form\CombinedServiceAgreementType;
+use App\Repository\CybersecurityAgreementRepository;
 use App\Repository\ServiceAgreementRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Query\QueryException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Entity\CybersecurityAgreement;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 #[Route('/admin/serviceagreements')]
 #[IsGranted('ROLE_ADMIN')]
 final class ServiceAgreementController extends AbstractController
 {
+    /**
+     * @throws QueryException
+     */
     #[Route(name: 'app_service_agreement_index', methods: ['GET'])]
-    public function index(ServiceAgreementRepository $serviceAgreementRepository): Response
+    public function index(ServiceAgreementRepository $serviceAgreementRepository, CybersecurityAgreementRepository $cybersecurityAgreementRepository): Response
     {
+        $serviceAgreements = $serviceAgreementRepository->findAll();
+        // Get all cybersecurity agreements indexed by ID
+        $cybersecurityAgreements = $cybersecurityAgreementRepository->findAllIndexed();
+
         return $this->render('service_agreement/index.html.twig', [
-            'service_agreements' => $serviceAgreementRepository->findAll(),
+            'service_agreements' => $serviceAgreements,
+            'cyber_security_agreements' => $cybersecurityAgreements,
         ]);
     }
-
     #[Route('/new', name: 'app_service_agreement_new', methods: ['GET', 'POST'])]
     public function new(Request $request, EntityManagerInterface $entityManager): Response
     {
         $serviceAgreement = new ServiceAgreement();
-        $form = $this->createForm(ServiceAgreementType::class, $serviceAgreement);
+        $cybersecurityAgreement = new CybersecurityAgreement();
+        $form = $this->createForm(CombinedServiceAgreementType::class, [
+            'serviceAgreement' => $serviceAgreement,
+            'cybersecurityAgreement' => $cybersecurityAgreement,
+        ]);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            $hasCybersecurityAgreement = $request->request->all('combined_service_agreement')['hasCybersecurityAgreement'] ?? false;
+
+            // First persist the ServiceAgreement
             $entityManager->persist($serviceAgreement);
-            $entityManager->flush();
+            $entityManager->flush(); // Flush to get the ID
+
+            if ($hasCybersecurityAgreement) {
+                // Set up the bidirectional relationship
+                $cybersecurityAgreement->setServiceAgreement($serviceAgreement);
+                $serviceAgreement->setCybersecurityAgreement($cybersecurityAgreement);
+
+                // Persist CybersecurityAgreement
+                $entityManager->persist($cybersecurityAgreement);
+                $entityManager->flush();
+            }
 
             return $this->redirectToRoute('app_service_agreement_index', [], Response::HTTP_SEE_OTHER);
         }
@@ -44,21 +71,37 @@ final class ServiceAgreementController extends AbstractController
         ]);
     }
 
-    #[Route('/{id}', name: 'app_service_agreement_show', methods: ['GET'])]
-    public function show(ServiceAgreement $serviceAgreement): Response
-    {
-        return $this->render('service_agreement/show.html.twig', [
-            'service_agreement' => $serviceAgreement,
-        ]);
-    }
-
     #[Route('/{id}/edit', name: 'app_service_agreement_edit', methods: ['GET', 'POST'])]
     public function edit(Request $request, ServiceAgreement $serviceAgreement, EntityManagerInterface $entityManager): Response
     {
-        $form = $this->createForm(ServiceAgreementType::class, $serviceAgreement);
+        $cybersecurityAgreement = $serviceAgreement->getCybersecurityAgreement() ?? new CybersecurityAgreement();
+        // Create the combined form
+        $form = $this->createForm(CombinedServiceAgreementType::class, [
+            'serviceAgreement' => $serviceAgreement,
+            'hasCybersecurityAgreement' => $serviceAgreement->getCybersecurityAgreement() !== null,
+            'cybersecurityAgreement' => $cybersecurityAgreement,
+        ]);
+
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            $hasCybersecurityAgreement = $request->request->all('combined_service_agreement')['hasCybersecurityAgreement'] ?? false;
+
+            if ($hasCybersecurityAgreement) {
+                if (!$serviceAgreement->getCybersecurityAgreement()) {
+                    // Set up the bidirectional relationship if it doesn't exist
+                    $cybersecurityAgreement->setServiceAgreement($serviceAgreement);
+                    $serviceAgreement->setCybersecurityAgreement($cybersecurityAgreement);
+                    $entityManager->persist($cybersecurityAgreement);
+                }
+            } else {
+                // Remove existing cybersecurity agreement if checkbox is unchecked
+                if ($serviceAgreement->getCybersecurityAgreement()) {
+                    $entityManager->remove($serviceAgreement->getCybersecurityAgreement());
+                    $serviceAgreement->setCybersecurityAgreement(null);
+                }
+            }
+
             $entityManager->flush();
 
             return $this->redirectToRoute('app_service_agreement_index', [], Response::HTTP_SEE_OTHER);
