6dcbb719cf9f6fce76f8bcfd972d8fb7e811a59f
Jeppe Krogh <kjej@aarhus.dk>
2025-11-03 07:32:14 +0100

    Added ApiKeyAuthenticator

---
+++ src/Security/ApiKeyAuthenticator.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Security;
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Core\Exception\CustomUserMessageAuthenticationException;
+use Symfony\Component\Security\Http\Authenticator\AbstractAuthenticator;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
+use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
+use Symfony\Component\Security\Http\Authenticator\Passport\SelfValidatingPassport;
+
+class ApiKeyAuthenticator extends AbstractAuthenticator
+{
+    private const string API_KEY_HEADER = 'X-Api-Key';
+
+    public function supports(Request $request): ?bool
+    {
+        return $request->headers->has(self::API_KEY_HEADER);
+    }
+
+    public function authenticate(Request $request): Passport
+    {
+        $apiToken = $request->headers->get(self::API_KEY_HEADER);
+        if (null === $apiToken) {
+            // The token header was empty, authentication fails with HTTP Status
+            // Code 401 "Unauthorized"
+            throw new CustomUserMessageAuthenticationException('No API key provided');
+        }
+
+        return new SelfValidatingPassport(new UserBadge($apiToken));
+    }
+
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): ?Response
+    {
+        // on success, let the request continue
+        return null;
+    }
+
+    public function onAuthenticationFailure(Request $request, AuthenticationException $exception): ?Response
+    {
+        $data = [
+            'message' => strtr($exception->getMessageKey(), $exception->getMessageData())
+        ];
+
+        return new JsonResponse($data, Response::HTTP_UNAUTHORIZED);
+    }
+}
