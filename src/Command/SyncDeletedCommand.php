d9fedd6023491406ef3e16f25b8153a53e991310
Troels Ugilt Jensen <6103205+tuj@users.noreply.github.com>
2025-11-05 11:58:49 +0100

    5538: Fixed issues from review

--- src/Command/SyncDeletedCommand.php
+++ src/Command/SyncDeletedCommand.php
@@ -6,14 +6,15 @@ use App\Service\LeantimeApiService;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Scheduler\Attribute\AsPeriodicTask;
 
 #[AsCommand(
     name: 'app:data-providers:sync-deleted',
-    description: 'Sync Data Provider deleted data, that has been deleted within the last hour, as jobs. Scheduled to run every 15 minutes.',
+    description: 'Sync Data Provider deleted data, that has been deleted within the given interval, as jobs. Run frequency can be set with the DATA_PROVIDER_DELETE_FREQUENCY environment variable.',
 )]
-#[AsPeriodicTask(frequency: '15 minutes')]
+#[AsPeriodicTask(frequency: '%env(DATA_PROVIDER_DELETE_FREQUENCY)%', jitter: 30)]
 class SyncDeletedCommand extends Command
 {
     public function __construct(
@@ -22,11 +23,18 @@ class SyncDeletedCommand extends Command
         parent::__construct($this->getName());
     }
 
+    protected function configure(): void
+    {
+        $this->addOption('interval', 'i', InputOption::VALUE_OPTIONAL, 'Only consider items deleted within the specified interval. See https://www.php.net/manual/en/dateinterval.construct.php for format.', 'PT1H');
+    }
+
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
+        $interval = $input->getOption('interval');
+
         // Look at entries modified within the last hour.
         $deletedAfter = new \DateTime();
-        $deletedAfter->sub(new \DateInterval('P1D'));
+        $deletedAfter->sub(new \DateInterval($interval));
 
         $this->leantimeApiService->updateAll(true, $deletedAfter);
 
