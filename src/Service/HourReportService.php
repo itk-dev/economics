d159aabe9d34d79c1debb1a5382e10eb4af6ad66
Jeppe Krogh <kjej@aarhus.dk>
2026-01-05 13:14:00 +0100

    Coding standards

--- src/Service/HourReportService.php
+++ src/Service/HourReportService.php
@@ -61,7 +61,7 @@ class HourReportService
 
             $epics = $issue->getEpics();
             // There should only be one tag per issue, but in case of multiple, we comma separate them in the list.
-            $issueEpicName = $epics->isEmpty() ? '' : implode(', ', $epics->map(fn($epic) => $epic->getTitle())->toArray());
+            $issueEpicName = $epics->isEmpty() ? '' : implode(', ', $epics->map(fn ($epic) => $epic->getTitle())->toArray());
 
             if ($hourReportData->projectTags->containsKey($issueEpicName)) {
                 $projectTag = $hourReportData->projectTags->get((string) $issueEpicName);
