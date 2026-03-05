fbbcfb2a7aceb86571840f3051d5ffddb8b56bd2
Troels Ugilt Jensen <6103205+tuj@users.noreply.github.com>
2026-03-05 20:25:30 +0100

    6855: Applied coding standards

--- src/Interface/DataProviderInterface.php
+++ src/Interface/DataProviderInterface.php
@@ -8,7 +8,7 @@ interface DataProviderInterface
      * Update all data related to instances of the given DataProvider.
      *
      * @param bool                    $asyncJobQueue handle as asynchronous jobs
-     * @param \DateTimeInterface|null $modifiedAfter only update elements modified after this date.
+     * @param \DateTimeInterface|null $modifiedAfter only update elements modified after this date
      */
     public function updateAll(bool $asyncJobQueue = false, ?\DateTimeInterface $modifiedAfter = null): void;
 
@@ -17,7 +17,7 @@ interface DataProviderInterface
      *
      * @param string                  $className     the className of the entity to update
      * @param bool                    $asyncJobQueue handle as asynchronous jobs
-     * @param \DateTimeInterface|null $modifiedAfter only update elements modified after this date.
+     * @param \DateTimeInterface|null $modifiedAfter only update elements modified after this date
      */
     public function update(string $className, bool $asyncJobQueue = false, ?\DateTimeInterface $modifiedAfter = null): void;
 }
