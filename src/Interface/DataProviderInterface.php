e0779d98be94d6b760862512b15a23807cabcb2d
Troels Ugilt Jensen <6103205+tuj@users.noreply.github.com>
2026-03-05 20:15:32 +0100

    6855: Added extra parameters

--- src/Interface/DataProviderInterface.php
+++ src/Interface/DataProviderInterface.php
@@ -7,15 +7,17 @@ interface DataProviderInterface
     /**
      * Update all data related to instances of the given DataProvider.
      *
-     * @param bool $asyncJobQueue handle as asynchronous jobs
+     * @param bool                    $asyncJobQueue handle as asynchronous jobs
+     * @param \DateTimeInterface|null $modifiedAfter only update elements modified after this date.
      */
     public function updateAll(bool $asyncJobQueue = false, ?\DateTimeInterface $modifiedAfter = null): void;
 
     /**
      * Update $className related to instances of the given DataProvider.
      *
-     * @param string $className     the className of the entity to update
-     * @param bool   $asyncJobQueue handle as asynchronous jobs
+     * @param string                  $className     the className of the entity to update
+     * @param bool                    $asyncJobQueue handle as asynchronous jobs
+     * @param \DateTimeInterface|null $modifiedAfter only update elements modified after this date.
      */
     public function update(string $className, bool $asyncJobQueue = false, ?\DateTimeInterface $modifiedAfter = null): void;
 }
