06e5ce519959efa2c4a6ed5b1116743c146da14a
Jeppe Krogh <kjej@aarhus.dk>
2026-04-10 11:24:16 +0200

    Add new fields and modify existing on ServiceAgreement

--- src/Entity/ServiceAgreement.php
+++ src/Entity/ServiceAgreement.php
@@ -3,12 +3,16 @@
 namespace App\Entity;
 
 use App\Enum\HostingProviderEnum;
+use App\Enum\ServerSizeEnum;
 use App\Enum\SystemOwnerNoticeEnum;
 use App\Repository\ServiceAgreementRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Validator\Context\ExecutionContextInterface;
 
 #[ORM\Entity(repositoryClass: ServiceAgreementRepository::class)]
+#[ORM\HasLifecycleCallbacks]
 class ServiceAgreement
 {
     #[ORM\Id]
@@ -44,14 +48,44 @@ class ServiceAgreement
     #[ORM\Column(type: Types::DATETIME_MUTABLE)]
     private ?\DateTimeInterface $validFrom = null;
 
-    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    #[ORM\Column(type: Types::DATETIME_MUTABLE, nullable: true)]
     private ?\DateTimeInterface $validTo = null;
 
     #[ORM\Column]
     private ?bool $isActive = null;
 
-    #[ORM\Column(enumType: SystemOwnerNoticeEnum::class)]
-    private ?SystemOwnerNoticeEnum $SystemOwnerNotice = null;
+    #[ORM\Column(type: Types::JSON)]
+    private array $systemOwnerNotices = [];
+
+    #[ORM\Column]
+    private bool $isEol = false;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $leantimeUrl = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $clientContactName = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $clientContactEmail = null;
+
+    #[ORM\Column]
+    private bool $dedicatedServer = false;
+
+    #[ORM\Column(enumType: ServerSizeEnum::class, nullable: true)]
+    private ?ServerSizeEnum $serverSize = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?float $cybersecurityPrice = null;
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $gitRepos = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $createdAt = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $updatedAt = null;
 
     public function getId(): ?int
     {
@@ -159,7 +193,7 @@ class ServiceAgreement
         return $this->validTo;
     }
 
-    public function setValidTo(\DateTimeInterface $validTo): static
+    public function setValidTo(?\DateTimeInterface $validTo): static
     {
         $this->validTo = $validTo;
 
@@ -178,15 +212,156 @@ class ServiceAgreement
         return $this;
     }
 
-    public function getSystemOwnerNotice(): ?SystemOwnerNoticeEnum
+    /**
+     * @return SystemOwnerNoticeEnum[]
+     */
+    public function getSystemOwnerNotices(): array
     {
-        return $this->SystemOwnerNotice;
+        return array_map(
+            fn (string $value) => SystemOwnerNoticeEnum::from($value),
+            $this->systemOwnerNotices
+        );
     }
 
-    public function setSystemOwnerNotice(SystemOwnerNoticeEnum $SystemOwnerNotice): static
+    /**
+     * @param SystemOwnerNoticeEnum[] $notices
+     */
+    public function setSystemOwnerNotices(array $notices): static
     {
-        $this->SystemOwnerNotice = $SystemOwnerNotice;
+        $this->systemOwnerNotices = array_map(
+            fn (SystemOwnerNoticeEnum $notice) => $notice->value,
+            $notices
+        );
 
         return $this;
+    }
+
+    public function isEol(): bool
+    {
+        return $this->isEol;
+    }
+
+    public function setIsEol(bool $isEol): static
+    {
+        $this->isEol = $isEol;
+
+        return $this;
+    }
+
+    public function getLeantimeUrl(): ?string
+    {
+        return $this->leantimeUrl;
+    }
+
+    public function setLeantimeUrl(?string $leantimeUrl): static
+    {
+        $this->leantimeUrl = $leantimeUrl;
+
+        return $this;
+    }
+
+    public function getClientContactName(): ?string
+    {
+        return $this->clientContactName;
+    }
+
+    public function setClientContactName(?string $clientContactName): static
+    {
+        $this->clientContactName = $clientContactName;
+
+        return $this;
+    }
+
+    public function getClientContactEmail(): ?string
+    {
+        return $this->clientContactEmail;
+    }
+
+    public function setClientContactEmail(?string $clientContactEmail): static
+    {
+        $this->clientContactEmail = $clientContactEmail;
+
+        return $this;
+    }
+
+    public function isDedicatedServer(): bool
+    {
+        return $this->dedicatedServer;
+    }
+
+    public function setDedicatedServer(bool $dedicatedServer): static
+    {
+        $this->dedicatedServer = $dedicatedServer;
+
+        return $this;
+    }
+
+    public function getServerSize(): ?ServerSizeEnum
+    {
+        return $this->serverSize;
+    }
+
+    public function setServerSize(?ServerSizeEnum $serverSize): static
+    {
+        $this->serverSize = $serverSize;
+
+        return $this;
+    }
+
+    public function getCybersecurityPrice(): ?float
+    {
+        return $this->cybersecurityPrice;
+    }
+
+    public function setCybersecurityPrice(?float $cybersecurityPrice): static
+    {
+        $this->cybersecurityPrice = $cybersecurityPrice;
+
+        return $this;
+    }
+
+    public function getGitRepos(): ?string
+    {
+        return $this->gitRepos;
+    }
+
+    public function setGitRepos(?string $gitRepos): static
+    {
+        $this->gitRepos = $gitRepos;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->createdAt;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeInterface
+    {
+        return $this->updatedAt;
+    }
+
+    #[ORM\PrePersist]
+    public function setCreatedAtValue(): void
+    {
+        $this->createdAt = new \DateTime();
+        $this->updatedAt = new \DateTime();
+    }
+
+    #[ORM\PreUpdate]
+    public function setUpdatedAtValue(): void
+    {
+        $this->updatedAt = new \DateTime();
+    }
+
+    #[Assert\Callback]
+    public function validateValidTo(ExecutionContextInterface $context): void
+    {
+        if ($this->isEol && null === $this->validTo) {
+            $context->buildViolation('service_agreement.valid_to_required_when_eol')
+                ->atPath('validTo')
+                ->addViolation();
+        }
     }
 }
