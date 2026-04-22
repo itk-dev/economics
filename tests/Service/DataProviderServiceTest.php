c38ae1f4de715d9f06fd648ac975c401c70d1008
Jeppe Krogh <kjej@aarhus.dk>
2026-04-22 13:04:49 +0200

    Simplified and corrected tests

--- tests/Service/DataProviderServiceTest.php
+++ tests/Service/DataProviderServiceTest.php
@@ -5,189 +5,105 @@ namespace App\Tests\Service;
 use App\Entity\DataProvider;
 use App\Entity\Issue;
 use App\Entity\Project;
+use App\Entity\Worklog;
+use App\Enum\BillableKindsEnum;
 use App\Enum\IssueStatusEnum;
 use App\Model\DataProvider\DataProviderIssueData;
-use App\Repository\DataProviderRepository;
-use App\Repository\EpicRepository;
 use App\Repository\IssueRepository;
-use App\Repository\ProjectRepository;
-use App\Repository\VersionRepository;
-use App\Repository\WorkerRepository;
 use App\Repository\WorklogRepository;
 use App\Service\DataProviderService;
+use App\Service\LeantimeApiService;
 use Doctrine\ORM\EntityManagerInterface;
-use PHPUnit\Framework\TestCase;
-use Psr\Container\ContainerInterface;
-use Psr\Log\LoggerInterface;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
-class DataProviderServiceTest extends TestCase
+class DataProviderServiceTest extends KernelTestCase
 {
-    private EntityManagerInterface $entityManager;
-    private ProjectRepository $projectRepository;
-    private IssueRepository $issueRepository;
-    private WorklogRepository $worklogRepository;
-    private DataProviderRepository $dataProviderRepository;
-    private LoggerInterface $logger;
-    private DataProviderService $service;
-
-    protected function setUp(): void
+    public function testUpsertIssueProjectChangeMovesWorklogs(): void
     {
-        $this->entityManager = $this->createMock(EntityManagerInterface::class);
-        $this->projectRepository = $this->createMock(ProjectRepository::class);
-        $this->issueRepository = $this->createMock(IssueRepository::class);
-        $this->worklogRepository = $this->createMock(WorklogRepository::class);
-        $this->dataProviderRepository = $this->createMock(DataProviderRepository::class);
-        $this->logger = $this->createMock(LoggerInterface::class);
+        self::bootKernel();
+        $container = self::getContainer();
 
-        $this->service = new DataProviderService(
-            $this->entityManager,
-            $this->projectRepository,
-            $this->issueRepository,
-            $this->worklogRepository,
-            $this->dataProviderRepository,
-            $this->createMock(VersionRepository::class),
-            $this->createMock(WorkerRepository::class),
-            $this->createMock(EpicRepository::class),
-            $this->createMock(ContainerInterface::class),
-            $this->logger,
-            25.0,
-            34.5,
-            '//',
-        );
-    }
+        $entityManager = $container->get(EntityManagerInterface::class);
+        $worklogRepository = $container->get(WorklogRepository::class);
+        $issueRepository = $container->get(IssueRepository::class);
+        $service = $container->get(DataProviderService::class);
 
-    public function testUpsertIssueProjectChangeUpdatesWorklogs(): void
-    {
-        $dataProvider = $this->createMock(DataProvider::class);
-        $dataProvider->method('getId')->willReturn(1);
-        $this->dataProviderRepository->method('find')->with(1)->willReturn($dataProvider);
+        // Create data provider.
+        $dataProvider = new DataProvider();
+        $dataProvider->setName('Test Provider - Worklog Move');
+        $dataProvider->setEnabled(true);
+        $dataProvider->setClass(LeantimeApiService::class);
+        $dataProvider->setUrl('www.example.com');
+        $dataProvider->setSecret('secret');
+        $entityManager->persist($dataProvider);
 
-        $projectA = $this->createMock(Project::class);
-        $projectA->method('getId')->willReturn(10);
-        $projectA->method('getProjectTrackerId')->willReturn('proj-A');
+        // Create project A and B.
+        $projectA = new Project();
+        $projectA->setDataProvider($dataProvider);
+        $projectA->setName('Project A');
+        $projectA->setProjectTrackerId('proj-a');
+        $projectA->setProjectTrackerKey('proj-a');
+        $projectA->setProjectTrackerProjectUrl('www.example.comproj-a');
+        $entityManager->persist($projectA);
 
-        $projectB = $this->createMock(Project::class);
-        $projectB->method('getId')->willReturn(20);
-        $projectB->method('getProjectTrackerId')->willReturn('proj-B');
+        $projectB = new Project();
+        $projectB->setDataProvider($dataProvider);
+        $projectB->setName('Project B');
+        $projectB->setProjectTrackerId('proj-b');
+        $projectB->setProjectTrackerKey('proj-b');
+        $projectB->setProjectTrackerProjectUrl('www.example.comproj-b');
+        $entityManager->persist($projectB);
 
-        $issue = new Issue();
-        $issue->setProject($projectA);
-        $issue->setDataProvider($dataProvider);
+        // Create issue C on project A.
+        $issueC = new Issue();
+        $issueC->setDataProvider($dataProvider);
+        $issueC->setProject($projectA);
+        $issueC->setProjectTrackerId('issue-c');
+        $issueC->setProjectTrackerKey('issue-c');
+        $issueC->setName('Issue C');
+        $issueC->setStatus(IssueStatusEnum::NEW);
+        $issueC->setLinkToIssue('www.example.comissue-c');
+        $entityManager->persist($issueC);
 
-        $this->issueRepository->method('findOneBy')->willReturn($issue);
-        $this->projectRepository->method('findOneBy')->willReturn($projectB);
+        // Create worklogs D and E linked to issue C and project A.
+        $worklogD = new Worklog();
+        $worklogD->setDataProvider($dataProvider);
+        $worklogD->setProject($projectA);
+        $worklogD->setIssue($issueC);
+        $worklogD->setWorklogId(100);
+        $worklogD->setWorker('test@test');
+        $worklogD->setTimeSpentSeconds(3600);
+        $worklogD->setStarted(new \DateTime());
+        $worklogD->setProjectTrackerIssueId('issue-c');
+        $worklogD->setDescription('Worklog D');
+        $worklogD->setKind(BillableKindsEnum::GENERAL_BILLABLE);
+        $entityManager->persist($worklogD);
 
-        $this->worklogRepository
-            ->expects($this->once())
-            ->method('updateProjectByIssue')
-            ->with($issue, $projectB)
-            ->willReturn(3);
+        $worklogE = new Worklog();
+        $worklogE->setDataProvider($dataProvider);
+        $worklogE->setProject($projectA);
+        $worklogE->setIssue($issueC);
+        $worklogE->setWorklogId(101);
+        $worklogE->setWorker('test@test');
+        $worklogE->setTimeSpentSeconds(7200);
+        $worklogE->setStarted(new \DateTime());
+        $worklogE->setProjectTrackerIssueId('issue-c');
+        $worklogE->setDescription('Worklog E');
+        $worklogE->setKind(BillableKindsEnum::GENERAL_BILLABLE);
+        $entityManager->persist($worklogE);
 
-        $this->service->upsertIssue($this->createIssueData(1, 'proj-B'));
-    }
+        $entityManager->flush();
 
-    public function testUpsertIssueNewIssueSkipsWorklogUpdate(): void
-    {
-        $dataProvider = $this->createMock(DataProvider::class);
-        $dataProvider->method('getId')->willReturn(1);
-        $this->dataProviderRepository->method('find')->with(1)->willReturn($dataProvider);
+        // Verify worklogs are on project A.
+        $this->assertEquals('proj-a', $worklogD->getProject()->getProjectTrackerId());
+        $this->assertEquals('proj-a', $worklogE->getProject()->getProjectTrackerId());
 
-        $projectB = $this->createMock(Project::class);
-        $projectB->method('getId')->willReturn(20);
-
-        $this->issueRepository->method('findOneBy')->willReturn(null);
-        $this->projectRepository->method('findOneBy')->willReturn($projectB);
-
-        $this->worklogRepository
-            ->expects($this->never())
-            ->method('updateProjectByIssue');
-
-        $this->service->upsertIssue($this->createIssueData(1, 'proj-B'));
-    }
-
-    public function testUpsertIssueSameProjectSkipsWorklogUpdate(): void
-    {
-        $dataProvider = $this->createMock(DataProvider::class);
-        $dataProvider->method('getId')->willReturn(1);
-        $this->dataProviderRepository->method('find')->with(1)->willReturn($dataProvider);
-
-        $projectA = $this->createMock(Project::class);
-        $projectA->method('getId')->willReturn(10);
-
-        $issue = new Issue();
-        $issue->setProject($projectA);
-        $issue->setDataProvider($dataProvider);
-
-        $this->issueRepository->method('findOneBy')->willReturn($issue);
-        $this->projectRepository->method('findOneBy')->willReturn($projectA);
-
-        $this->worklogRepository
-            ->expects($this->never())
-            ->method('updateProjectByIssue');
-
-        $this->service->upsertIssue($this->createIssueData(1, 'proj-A'));
-    }
-
-    public function testUpsertIssueNullProjectSkipsWorklogUpdate(): void
-    {
-        $dataProvider = $this->createMock(DataProvider::class);
-        $dataProvider->method('getId')->willReturn(1);
-        $this->dataProviderRepository->method('find')->with(1)->willReturn($dataProvider);
-
-        $projectA = $this->createMock(Project::class);
-        $projectA->method('getId')->willReturn(10);
-
-        $issue = new Issue();
-        $issue->setProject($projectA);
-        $issue->setDataProvider($dataProvider);
-
-        $this->issueRepository->method('findOneBy')->willReturn($issue);
-        $this->projectRepository->method('findOneBy')->willReturn(null);
-
-        $this->worklogRepository
-            ->expects($this->never())
-            ->method('updateProjectByIssue');
-
-        $this->service->upsertIssue($this->createIssueData(1, 'non-existent-project'));
-    }
-
-    public function testUpsertIssueProjectChangeWithNoWorklogs(): void
-    {
-        $dataProvider = $this->createMock(DataProvider::class);
-        $dataProvider->method('getId')->willReturn(1);
-        $this->dataProviderRepository->method('find')->with(1)->willReturn($dataProvider);
-
-        $projectA = $this->createMock(Project::class);
-        $projectA->method('getId')->willReturn(10);
-        $projectA->method('getProjectTrackerId')->willReturn('proj-A');
-
-        $projectB = $this->createMock(Project::class);
-        $projectB->method('getId')->willReturn(20);
-        $projectB->method('getProjectTrackerId')->willReturn('proj-B');
-
-        $issue = new Issue();
-        $issue->setProject($projectA);
-        $issue->setDataProvider($dataProvider);
-
-        $this->issueRepository->method('findOneBy')->willReturn($issue);
-        $this->projectRepository->method('findOneBy')->willReturn($projectB);
-
-        $this->worklogRepository
-            ->expects($this->once())
-            ->method('updateProjectByIssue')
-            ->with($issue, $projectB)
-            ->willReturn(0);
-
-        $this->service->upsertIssue($this->createIssueData(1, 'proj-B'));
-    }
-
-    private function createIssueData(int $dataProviderId, string $projectTrackerProjectId): DataProviderIssueData
-    {
-        return new DataProviderIssueData(
-            projectTrackerId: 'issue-1',
-            dataProviderId: $dataProviderId,
-            projectTrackerProjectId: $projectTrackerProjectId,
-            name: 'Test Issue',
+        // Move issue C from project A to project B via upsertIssue.
+        $service->upsertIssue(new DataProviderIssueData(
+            projectTrackerId: 'issue-c',
+            dataProviderId: $dataProvider->getId(),
+            projectTrackerProjectId: 'proj-b',
+            name: 'Issue C',
             epics: [],
             plannedHours: 0.0,
             remainingHours: 0.0,
@@ -196,10 +112,23 @@ class DataProviderServiceTest extends TestCase
             dueDate: null,
             resolutionDate: null,
             fetchTime: new \DateTime(),
-            url: 'http://example.com/issue-1',
+            url: 'www.example.comissue-c',
             sourceModifiedDate: null,
             versionId: null,
             disableModifiedAtCheck: true,
-        );
+        ));
+
+        // Clear identity map to get fresh state from DB.
+        $entityManager->clear();
+
+        // Verify issue is now on project B.
+        $issue = $issueRepository->findOneBy(['projectTrackerId' => 'issue-c', 'dataProvider' => $dataProvider->getId()]);
+        $this->assertEquals('proj-b', $issue->getProject()->getProjectTrackerId());
+
+        // Verify both worklogs are now on project B.
+        $worklogD = $worklogRepository->findOneBy(['worklogId' => 100, 'dataProvider' => $dataProvider->getId()]);
+        $worklogE = $worklogRepository->findOneBy(['worklogId' => 101, 'dataProvider' => $dataProvider->getId()]);
+        $this->assertEquals('proj-b', $worklogD->getProject()->getProjectTrackerId());
+        $this->assertEquals('proj-b', $worklogE->getProject()->getProjectTrackerId());
     }
 }
