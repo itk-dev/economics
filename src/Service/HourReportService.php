d2d12460e72c384b332444d03c32c23b215a096e
Jeppe Krogh <kjej@aarhus.dk>
2026-05-01 09:18:31 +0200

    Sorted hour report by tag name

--- src/Service/HourReportService.php
+++ src/Service/HourReportService.php
@@ -12,6 +12,7 @@ use App\Model\Reports\HourReportProjectTicket;
 use App\Model\Reports\HourReportWorklog;
 use App\Repository\IssueRepository;
 use App\Repository\WorklogRepository;
+use Doctrine\Common\Collections\ArrayCollection;
 
 class HourReportService
 {
@@ -84,6 +85,10 @@ class HourReportService
             $hourReportData->projectTotalEstimated += $totalTicketEstimated;
             $hourReportData->projectTotalSpent += $totalTicketSpent;
         }
+
+        $tagsIterator = $hourReportData->projectTags->getIterator();
+        $tagsIterator->uasort(fn ($a, $b) => mb_strtolower($a->tag) <=> mb_strtolower($b->tag));
+        $hourReportData->projectTags = new ArrayCollection(iterator_to_array($tagsIterator));
 
         return $hourReportData;
     }
