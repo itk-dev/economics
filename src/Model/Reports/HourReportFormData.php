356eee8c5254519668bdbe153e877c6bf3165e1c
Jeppe Krogh <kjej@aarhus.dk>
2026-05-20 12:04:05 +0200

    Remove data-provider filter from hour report

--- src/Model/Reports/HourReportFormData.php
+++ src/Model/Reports/HourReportFormData.php
@@ -2,13 +2,11 @@
 
 namespace App\Model\Reports;
 
-use App\Entity\DataProvider;
 use App\Entity\Project;
 use App\Entity\Version;
 
 class HourReportFormData
 {
-    public DataProvider $dataProvider;
     public Project $project;
     public Version $version;
     public \DateTimeInterface $fromDate;
