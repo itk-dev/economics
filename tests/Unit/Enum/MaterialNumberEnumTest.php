02004bf18822c88beea0609055e4cb536e15b03a
Jeppe Krogh <kjej@aarhus.dk>
2026-09-01 10:53:51 +0200

    Replaced a tautological enum assertion with a data provider

--- tests/Unit/Enum/MaterialNumberEnumTest.php
+++ tests/Unit/Enum/MaterialNumberEnumTest.php
@@ -28,11 +28,32 @@ class MaterialNumberEnumTest extends TestCase
         ];
     }
 
-    public function testBackingValuesAreTheAgreedMaterialNumbers(): void
+    /**
+     * @dataProvider materialNumberProvider
+     */
+    public function testBackingValueIsTheAgreedMaterialNumber(MaterialNumberEnum $case, string $expected): void
+    {
+        $this->assertSame($expected, $case->value);
+    }
+
+    /**
+     * @return array<string, array{MaterialNumberEnum, string}>
+     */
+    public static function materialNumberProvider(): array
+    {
+        return [
+            'none' => [MaterialNumberEnum::NONE, ''],
+            'internal' => [MaterialNumberEnum::INTERNAL, '103361'],
+            'external with moms' => [MaterialNumberEnum::EXTERNAL_WITH_MOMS, '100006'],
+            'external without moms' => [MaterialNumberEnum::EXTERNAL_WITHOUT_MOMS, '100008'],
+        ];
+    }
+
+    public function testTheProvidersCoverEveryCase(): void
     {
         $this->assertSame(
-            ['', '103361', '100006', '100008'],
-            array_map(fn (MaterialNumberEnum $case) => $case->value, MaterialNumberEnum::cases())
+            array_map(fn (array $row) => $row[0], array_values(self::materialNumberProvider())),
+            MaterialNumberEnum::cases()
         );
     }
 }
